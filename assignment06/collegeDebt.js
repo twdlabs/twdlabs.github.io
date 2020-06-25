8258479b600b1bf5f1c88b9d23433d995f13506e
atg973 <alextg6@gmail.com>
2020-06-25 16:42:32 -0400

    Add files via upload

--- assignment06/collegeDebt.js
+++ assignment06/collegeDebt.js
@@ -1,5 +1,6 @@
 
 // --- Global Variables --- //
+
 var loans = [
 	{ loan_year:2020, loan_amount:10000, loan_int_rate:0.0453 },
 	{ loan_year:2021, loan_amount:10000, loan_int_rate:0.0453 },
@@ -9,7 +10,8 @@ var loans = [
 ];
 
 
-
+var loanBalance;
+var paymentData;
 
 // Format a given number value (with commas separating groups of 3 digits). 
 function toCommas(value) {
@@ -146,31 +148,60 @@ function loadDoc() {
 // Update the loans data with user-entered values. 
 function updateLoansArray() {
 	// Validate user input using regular expressions. RegEx Tester: https://www.regexpal.com/
-	var valid = true;									// Valid by default until regex detects mismatch. 
-	var regexYr = /^(19|20)\d{2}$/;						// Enforces: 4 numerical digits staring with 19 or 20
-	var regexAmt = /^([1-9][0-9]*)+(.[0-9]{1,2})?$/;	// Enforces: ?
-	var regexInt = /^(0|)+(.[0-9]{1,5})?$/;				// Enforces: ?
+	var valid = true;										// Valid by default until regex detects mismatch. 
+	var regexYear = /^(19|20)\d{2}$/;						// Enforces rule: Four numerical digits staring with 19 or 20
+	var regexLoanAmt = /^([1-9][0-9]*)+(.[0-9]{1,2})?$/;	// Enforces rule: ?
+	var regexIntRate = /^(0|)+(.[0-9]{1,5})?$/;				// Enforces rule: Start with '0.' or '.', followed by 1-5 digits
+
+	// Prevent update and turn red if user-entered year doesn't match regex. 
+	if( !regexYear.test( $(`#loan_year01`).val() ) ) {
+		$('#loan_year01').css('background-color','#f33');
+		valid = false;
+	}
+
+	// Prevent update and turn red if user-entered loan amount doesn't match regex. 
+	for (var i=0 ; i<loans.length ; i++) {
+		if( !regexLoanAmt.test( $(`#loan_amt0${i+1}`).val() ) ) {
+			$(`#loan_amt0${i}`).css('background-color','#f33');
+			valid = false;
+		}
+	}
+
+	// Prevent update and turn red if user-entered interest rate doesn't match regex. 
+	if( !regexIntRate.test( $(`#loan_int01`).val() ) ) {
+		$('#loan_int01').css('background-color','#f33');
+		valid = false;
+	}
 
 	// Proceed if user-enetered data is valid. 
 	if(valid) {
-		// Update the loan data in array (years, amounts, interest rates). 
+
+		// Save first loan year in array
+		loans[0].loan_year = parseInt( $('#loan_year01').val() );
+
+		// Get interest rate from firs year. 
+		let intRate = parseFloat( $('#loan_int01').val() );
+
+		// Update loan data in array (years, amounts, interest rates). 
 		for(var i=0 ; i<loans.length ; i++) {
 			// Save year in array. 
-			if(i==0) loans[i].loan_year = parseInt( $('#loan_year01').val() );
-			else loans[i].loan_year = loans[0].loan_year + i;
+			loans[i].loan_year = loans[0].loan_year + i;
 
 			// Save loan amount in array. 
-			loans[i].loan_amount = parseInt(  $( '#loan_amt0'+(i+1) ).val()  );
+			loans[i].loan_amount = parseFloat( $(`#loan_amt0${i+1}`).val() ).toFixed(2);
 
 			// Save interest rate in array. 
-			loans[i].loan_int_rate = Number.parseFloat(  $('#loan_int01').val()  );
+			loans[i].loan_int_rate = intRate;
 		}
 
 		// Log the updating activity in the console. 
-		console.log( 'Updated loan data:\n'+JSON.stringify(loans) );
+		console.log( 'Updated loan data in array.' );
 
 		// Display updated loans data on the form. 
 		updateForm();
+
+		// Log the updating activity in the console. 
+		console.log( 'Updated loan data on form.' );
 	}
 }
 
@@ -178,29 +209,32 @@ function updateLoansArray() {
 
 
 
-// Display updated loans data on the form (using loan data in array). 
+// Display updated data on the form (using loan data in array). 
 function updateForm() {
-	// Enforces rule: four numerical digits starting with 19 or 20.
-	var yearP = /^(19|20)\d{2}$/;
-	var amtP = /^([1-9][0-9]*)+(.[0-9]{1,2})?$/;
-	var intP = /^$/;
+	var loanPrincipal = 0;
+	loanBalance = 0;
 
-	if( yearP.test( $().val() ) );
-
-	var loanBalance = 0;
 	// For every year of the loan, ...
 	for (var i=0 ; i<loans.length ; i++) {
 		// Update loan year. 
-		$('#loan_year0'+(i+1)).val( loans[i].loan_year );
-		// Update loan amount. 
-		$('#loan_amt0'+(i+1)).val( loans[i].loan_amount.toFixed(2) );
+		$(`#loan_year0${i+1}`).val( loans[i].loan_year );
+
+		// Update new loan amount. 
+		let newLoan = parseFloat( loans[i].loan_amount );
+		$(`#loan_amt0${i+1}`).val( newLoan );
+		loanPrincipal += newLoan;
+
 		// Update loan interest rate. 
-		$('#loan_int0'+(i+1)).val( loans[i].loan_int_rate );
-		// Calculate year-end loan balance. 
-		loanBalance += 1*loans[i].loan_amount * (1 + 1*loans[i].loan_int_rate);
+		let rate = parseFloat( loans[i].loan_int_rate );
+		$(`#loan_int0${i+1}`).val( rate );
+
 		// Update year-end loan balance. 
-		$('#loan_bal0'+(i+1)).html( toCommas(loanBalance.toFixed(2)) );
+		loanBalance = (loanBalance + newLoan) * (1 + rate);
+		$(`#loan_bal0${i+1}`).text( toMoney(loanBalance.toFixed(2)) );
 	}
+
+	// Display the total amount of interest accrued while in school. 
+	$(`#loan_int_accrued`).text( toMoney(loanBalance - loanPrincipal) );
 }
 
 
@@ -210,30 +244,66 @@ function updateForm() {
 // ANGULAR //
 /***********/
 
-// var app = angular.module('myApp',[]);
+var app = angular.module('myApp', []);
 
-// app.controller('myCtrl', function($scope){
-// 	$scope.payments = [];
-// 	$scope.populate = function() {
-// 		updateForm();
-// 		let total = ;
+app.controller('myCtrl', function($scope) {
+	$scope.payments = [];
 
-// 		let pay = 12 * (total / ((( ))) )
-// 	}
-// } );
+	$scope.populate = function() {
+		// Update the form prior to calculating payments. 
+		updateForm();
+
+		// Get principal amount (loan balance at start of payment period). 
+		let currentBalance = loanBalance;
+
+		// Get annual interest rate. 
+		let intRate = loans[0].loan_int_rate;
+
+		// Get monthly iterest rate. 
+		let r = intRate / 12;
+
+		// Set number of years for payment phase
+		let n = 11;
+
+		// Loan Payment Formula (https://www.thebalance.com/loan-payment-calculations-315564)
+		let paymentAmount = currentBalance * r * Math.pow(1+r,n) / (Math.pow(1+r,n)-1);
+		// A = P * i * Math.pow(1+i,n) / ( Math.pow(1+i,n) - 1 )
+
+		for (let i=0 ; i<n-1 ; i++) {
+			currentBalance -= paymentAmount;
+			let intAmount = currentBalance * intRate;
+			$scope.payments[i] = {
+				'year': (loans[loans.length-1].loan_year + 1 + i),
+				'payment': toMoney(paymentAmount),
+				'intAmount': toMoney(intAmount),
+				'ye': toMoney(currentBalance += intAmount)
+			}
+		}
+
+		$scope.payments[10] = {
+			'year': (loans[loans.length-1].loan_year + n),
+			'payment': toMoney(currentBalance),
+			'intAmount': toMoney(0),
+			'ye': toMoney(0)
+		}
+
+		paymentData = $scope.payments;
+	}
+
+} );
 
 
 
 
 // TODO: Take the user input, and use it to populate the payments table. 
-function processForm() {
-	var principalAmount = 0;
-	for (var i=0 ; i<loans.length ; i++) {
-		principalAmount += loans[i].loan_amount;
-		principalAmount *= (1 + loans[i].loan_int_rate);
-	}
+// function processForm() {
+// 	var currentBalance = 0;
+// 	for (var i=0 ; i<loans.length ; i++) {
+// 		currentBalance += loans[i].loan_amount;
+// 		currentBalance *= (1 + loans[i].loan_int_rate);
+// 	}
 
-	var paymentAmount = principalAmount;
-}
+// 	var paymentAmount = currentBalance;
+// }
 
 
