c067d6839db5c79bf77c3bee1212093e13b8e52e
A Titus <25465781+atg973@users.noreply.github.com>
2021-12-09 17:57:42 -0500

    Add files via upload

---
+++ projectNames.js
@@ -0,0 +1,89 @@
+//
+// // List page names.
+// var labels = ['Homepage','Projects In-Progress','Favorite Projects'];
+// var pageNames = [
+// 					'chooser', 'evolution', 'finance', 'fitness', 'math', 'music', 'probability', 'reast', 'reast2', 'stocks', 'tabs',
+// 					'acito', 'layouts', 'linktree',
+// 					'bgvid', 'buttonpress', 'form3d', 'git', 'hero', 'image', 'logo', 'logoanimation', 'parallax', 'parallax2', 'popup', 'storage', 'ticker',
+// 					'aspectratio', 'autocomplete', 'checkout', 'cluster', 'device', 'form', 'hero', 'lightSwitch', 'loading', 'nav', 'overlay', 'pricing', 'refresh', 'scale3d', 'scroller', 'scrollProgressBar', 'search', 'sidenav', 'slideshow', 'socialMedia', 'solarsystem', 'sphere', 'splash', 'sort', 'soundfx', 'syntax', 'template', 'user', 'viewport'
+// 				];
+// var pageNames2d = [
+// 					['chooser', 'evolution', 'finance', 'fitness', 'math', 'music', 'probability', 'reast', 'reast2', 'stocks', 'tabs'] ,
+// 					['acito', 'layouts', 'linktree'] ,
+// 					['bgvid', 'buttonpress', 'form3d', 'git', 'hero', 'image', 'logo', 'logoanimation', 'parallax', 'parallax2', 'popup', 'storage', 'ticker'] ,
+// 					['aspectratio', 'autocomplete', 'checkout', 'cluster', 'device', 'form', 'hero', 'lightSwitch', 'loading', 'nav', 'overlay', 'pricing', 'refresh', 'scale3d', 'scroller', 'scrollProgressBar', 'search', 'sidenav', 'slideshow', 'socialMedia', 'solarsystem', 'sphere', 'splash', 'sort', 'soundfx', 'syntax', 'template', 'user', 'viewport']
+// 				];
+// var pageUrls = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//
+
+
+// const projectPublicNames = [
+//
+// 	{	name:'Home',
+// 		url:'chooser'
+// 	},
+// 	{	name:'Finance',
+// 		url:'finance'
+// 	},
+// 	{	name:'Math',
+// 		url:'math'
+// 	},
+// 	{	name:'Music',
+// 		url:'music'
+// 	},
+// 	{	name:'Probability',
+// 		url:'probability'
+// 	},
+// 	{	name:'Real Estate',
+// 		url:'reast'
+// 	},
+// 	{	name:'Stocks',
+// 		url:'stocks'
+// 	}
+//
+// ];
+
+
+
+/*********************************************************/
+
+
+
+// List project names.
+const projectNames = [
+	'3d','10000cards',
+	'admin','affirm','agency','animation','apple','application','aspectratio','atg','audio','autocomplete',
+	'backToTop','band','battlerap','big3dcard','blog','bodyshop','book','books','box','bs5','bubblesort','buttonpress',
+	'calc','calculator','calendar','cali','caliB','canvas','cardflipgallery3d','carnell','carnell2','carousel','carousel2','carousel3d',
+	'chadpiff','chart','chat','checkout','chooser','chooser-old','cis255','clearfix','clock','clone','cluster','cluster1d','cms',
+	'code','cognitivebias','collapsible','color','comingsoon','contact','contactform','contextmenu', 'cookie','copyright','covid','cubecarousel','cutout',
+	'dashboard','db','decision','deck','depth','device','diagram','dice','dollar','draggable',
+	'eazechain','ecomm','ecommerce','editor','enter','essay','event','evolution','ezdev',
+	'fam','family','filter','finance','fitness','flex','flexbreak','font','form','form3d','format',
+	'gallerywalk','gametapes','goodz','gradient','graph','grid','grocery','grocery2','guess',
+	'hero','hero2','hovercard',
+	'icon','igt','image','input','instagram',
+	'jay','jenna','json',
+	'keyframes','keygenius','koan','kumba',
+	'land','layouts','leads','learner','levels','lightSwitch','linktree','loading','login','logo',
+	'macbook','math','matrix','media','mementomori','mentalmodels','message','minion','music',
+	'navbar','neuro67','news','notes',
+	'overflow','overlay',
+	'pageNexter','personality','perspective','phonecall','pinyin','plax','plax2','pokeball','pong','popup','portfolio','postercircle','pricing','probability','profilecard','progressbar','prototype',
+	'rating','reast','refresh','regexp','resume','robinhood',
+	'scale3d','scroller','scrollProgressBar','scrollsnap','search','shape','sidenav','signin','skills','sky','slide','slider','slideshow','smoothscroll','smosa',
+	'socialmedia','solarsystem','sort','soundfx','spades','sphere','spinner','splash','squarespace','stocks','storage','student','superforg','svsu','syntax',
+	'tabs','tabs3d','tagboard','tags','tasks','template','theme','ticker','toast','todo','topnav','traffic','travisscott','trifold3d','trig','tropicalghana','typetest','typewriter',
+	'univ','unseenstrts','urlparams','user',
+	'vanilla','video','viewport','vidbg','vidbg2',
+	'watchthescreen','waves','wordcube','wts',
+	'ytblp',
+	'zbar'
+];
+
+
+// List public project names.
+const projectPublicNames = [
+	'chooser','finance', 'math', 'music', 'probability', 'stocks',
+	'calc','cluster','tabs','db','diagram','flex','grid','igt','levels','matrix','mementomori','notes','phonecall','regexp','sidenav','tags','tasks','waves'
+];
