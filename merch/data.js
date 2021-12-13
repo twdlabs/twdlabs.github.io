3ad7e2377b3128a6496af184eb5166bbf630996a
A Titus <25465781+atg973@users.noreply.github.com>
2021-12-12 23:43:08 -0500

    Add files via upload

---
+++ merch/data.js
@@ -0,0 +1,152 @@
+
+
+// Initialize list of id's of cart items. 
+let cartIds = [0,1,1,1,2,2];
+
+// Define product data. 
+let productdata = [
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit amet consectetur adipisicing elit totam sapiente consectetur eligendi',
+		rating:{
+			average:5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/shoes.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/shoes1.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/tshirt.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/hat.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/sunglasses.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/p2.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/p3.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/p4.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/p5.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/p7.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/shoes.jpg',
+		producturl:'./product/'
+	},
+	{
+		name:'Lorem Ipsum Dolor',
+		description:'Lorem ipsum dolor sit, amet consectetur adipisicing',
+		rating:{
+			average:4.5,
+			numratings:137
+		},
+		price:'99.99',
+		exprice:'149.99',
+		photourl:'./pic/shoes1.jpg',
+		producturl:'./product/'
+	},
+];
--- merch/script.js
+++ merch/script.js
@@ -17,14 +17,6 @@ let navdata = [
 	{
 		name:'MenuItemName',
 		url:'javascript:void(0)'
-	},
-	{
-		name:'MenuItemName',
-		url:'javascript:void(0)'
-	},
-	{
-		name:'MenuItemName',
-		url:'javascript:void(0)'
 	}
 ];
 
@@ -33,7 +25,12 @@ let navdata = [
 
 
 // Load navigation bar from file. 
-// $(document).ready(function() {loadNav();});
+$(document).ready(function() {
+
+	// Update cart. 
+	updateCart();
+});
+
 
 
 /*****/
@@ -41,8 +38,12 @@ let navdata = [
 
 // Load navigation bar from file. 
 function loadNav(atRootDirectory) {
+
+	// Get relative url of navbar file. 
 	let url = ( (atRootDirectory) ? ('') : ('../') ) + 'navbar.html';
-	console.log('navbar url:',url);
+	console.log('Loading navbar:',url);
+
+	// Load navbar. 
 	$('#navbar').load(url);
 	// $('#navbar').load(url,populateNavList);
 
@@ -73,11 +74,146 @@ function populateNavList() {
 	document.getElementById('navlist').innerHTML = (result);
 }
 
+
+// Toggle navbar slide drawer. 
+function toggleDrawer(id) {
+
+	// Close all other drawers. 
+	closeOtherDrawers(id);
+	
+	// Toggle selected drawer. 
+	document.getElementById(id).classList.toggle('active');
+}
+
+// Open navbar slide drawer. 
+function openDrawer(id) {
+
+	// Close all other drawers. 
+	closeOtherDrawers(id);
+	
+	// Open selected drawer. 
+	document.getElementById(id).classList.add('active');
+}
+
 // Close all other navbar slide drawers. 
 function closeOtherDrawers(exceptionId) {
-	let ids = ['navlist','searchbar'];
-	for(id of ids) {
+
+	// Get all nav drawers. 
+	let navDrawers = document.querySelectorAll('.navdrawer');
+	navDrawers = [...navDrawers];
+	// console.log('navDrawers', navDrawers);
+
+	// Get id's for all nav drawers. 
+	// let drawerIds = ['navlist','searchbar','likebox','cartbox','accountbox'];
+	let drawerIds = navDrawers.map( (node) => (node.id) );
+	// console.log('drawerIds', drawerIds);
+
+	// De-activate all other drawers. 
+	for(id of drawerIds) {
 		// Set drawer as inactive. 
 		if(id!=exceptionId) document.getElementById(id).classList.remove('active');
 	}
 }
+
+
+/* SEARCH */
+
+// TODO: Search for products related to search query. 
+function searchForProduct() {
+	// Get search query. 
+	let query = document.getElementById('searchquery').value;
+
+	// Show search query. 
+	console.log('Search: '+query);
+	// alert('Search: '+query);
+
+	// TODO: Search for matching products. 
+
+	// TODO: Show search results page for matching products. 
+}
+
+
+/* LIKES */
+
+
+/* CART */
+
+// Add selected item to cart. 
+function addToCart(element) {
+	console.log('Now adding to cart:',element);
+
+	// Get product id of selected item. 
+	let id = element.getAttribute('data-productid');
+
+	// Add id of selected item to cart array. 
+	cartIds.push(id);
+
+	// Show items in cart. 
+	console.log('Cart:',cartIds);
+
+	// Update cart items in cart drawer. 
+	updateCart();
+
+	// Show cart drawer on page. 
+	openDrawer('cartbox');
+}
+
+// Update cart items in cart drawer. 
+function updateCart() {
+	
+	// Add element for each cart item. 
+	let result = '';
+	for(id of cartIds) {
+
+		// Get product by id. 
+		product = productdata[id];
+
+		// Create product element. 
+		result += `
+		<!-- item -->
+		<div class="item">
+
+			<!-- photo -->
+			<div class="photo">${ (product.photourl) ? (`<img src="${product.photourl}">`) : ('') }</div>
+			<!-- /photo -->
+
+			<!-- content -->
+			<div class="content">
+
+				<!-- name -->
+				<div class="name">${product.name}</div>
+				<!-- /name -->
+
+				<!-- desc -->
+				<div class="desc">${product.description}</div>
+				<!-- /desc -->
+
+			</div>
+			<!-- /content -->
+
+			<!-- quantity -->
+			<div class="quantity">
+				<span class="delta">&minus;</span>
+				<span class="qty">1</span>
+				<span class="delta">&plus;</span>
+			</div>
+			<!-- /quantity -->
+
+		</div>
+		<!-- /item -->`;
+	}
+	// console.log('result',result);
+
+	// Get inside of cart drawer. 
+	let cartbox = document.querySelector('div#cartbox div.inner');
+	// let cartbox = $('div#cartbox div.inner');
+	console.log('cartbox',cartbox);
+
+	// Add elements to cart drawer. 
+	cartbox.innerHTML = result;
+	// cartbox.html(result);
+}
+
+
+/* ACCOUNT */
+
