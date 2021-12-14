1ae25649ac891a5fb1e981ab9820c1d288460d84
A Titus <25465781+atg973@users.noreply.github.com>
2021-12-14 17:18:02 -0500

    Add files via upload

--- merch/script/search.js
+++ merch/script/search.js
@@ -2,14 +2,34 @@
 
 // TODO: Search for products related to search query. 
 function searchForProduct() {
-	// Get search query. 
-	let query = document.getElementById('searchquery').value;
+
+	// Initialize list of ids for matching products. 
+	let matchingProductIds = [];
+
+	// Get search query from search field. 
+	let searchfield = document.getElementById('searchquery')
+	let query = searchfield.value;
+
+	// Clear search field and remove focus. 
+	searchfield.value = '';
+	searchfield.blur();
+	closeOtherDrawers();
 
 	// Show search query. 
-	console.log('Search: '+query);
-	// alert('Search: '+query);
+	console.log('Search query: '+query);
 
-	// TODO: Search for matching products. 
+	// Get all matching products. 
+	for(key in productdata) {
 
-	// TODO: Show search results page for matching products. 
+		// Get product data item. 
+		p = productdata[key];
+
+		// Add to list of matching products if name or descriptions matches query. 
+		if( p.name.includes(query) || p.description.includes(query) ) matchingProductIds.push(key);
+	}
+
+	// TODO: Show results popup page with matching products. 
+	console.log('All products:',productdata);
+	console.log('matchingProductIds',matchingProductIds);
+	alert('Search results for: '+query + '\n' + ( matchingProductIds.length ? matchingProductIds : '[none]' ) );
 }
