33d7334ac2b0fdb3630160054736bb151963d95c
A Titus <25465781+atg973@users.noreply.github.com>
2021-12-22 17:24:26 -0500

    Add files via upload

--- projectNames.js
+++ projectNames.js
@@ -1,3 +1,45 @@
+
+
+// List project names.
+const projectNames = [
+	'3d','10000cards',
+	'accordion','admin','affirm','agency','animation','apple','application','aspectratio','atg','audio','autocomplete','autoslideshow',
+	'backToTop','band','battlerap','big3dcard','blog','bodyshop','book','bookmark','books','box','bs5','bubblesort','buttonpress',
+	'calc','calculator','calendar','cali','caliB','canvas','cardflipgallery3d','carnell','carnell2','carousel','carousel3d',
+	'chadpiff','chart','chat','checkout','checkout2','chooser','chooser-old','cis255','clearfix','clock','clone','cluster','cluster1d','cms',
+	'code','cognitivebias','collapsible','color','comingsoon','contact','contactform','contextmenu', 'cookie','copyright','covid','cubecarousel','cutout',
+	'dashboard','db','decision','deck','depth','device','diagram','dice','dollar','dotcollector','draggable',
+	'eazechain','editor','enter','essay','event','evolution','ezdev',
+	'fam','family','filter','finance','fitness','flashlight','flex','flexbreak','font','form','form3d','format',
+	'gallerywalk','gametapes','goodz','gooey','gradient','graph','grid','grocery','grocery2','guess',
+	'hero','hero2','hexagon','hovercard',
+	'icon','igt','image','imagefilter','input','instagram',
+	'jay','jenna','json',
+	'keyframes','keygenius','koan','kumba',
+	'land','layouts','leads','learner','levels','lightSwitch','linktree','iglive','loading','login','logo',
+	'macbook','maskimage','masonry','math','matrix','media','mementomori','mentalmodels','merch','message','minion','music',
+	'navbar','neuro67','news','notepad','notes',
+	'overflow','overlay',
+	'pageNexter','personality','perspective','phonecall','pinyin','plax','plax2','pokeball','pong','popup','portfolio','postercircle','pricing','probability','profilecard','progressbar','prototype',
+	'rating','readmore','reast','refresh','regexp','resume','robinhood',
+	'scale3d','scroller','scrollProgressBar','scrollsnap','search','select','shape','sidenav','signin','skills','sky','slide','slider','slideshow','smoothscroll','smosa',
+	'socialmedia','solarsystem','sort','soundfx','spades','sphere','spinner','splash','squarespace','stocks','storage','student','superforg','svsu','syntax',
+	'tabs','tabs3d','tagboard','tags','tasks','template','theme','theme2','ticker','timeline','toast','todo','topnav','traffic','travisscott','trifold3d','trig','tropicalghana','typer','typetest','typewriter',
+	'univ','unseenstrts','urlparams','user',
+	'vanilla','video','viewport','vidbg','vidbg2','voisey',
+	'watchthescreen','waves','wordcube','wts',
+	'ytblp',
+	'zbar'
+];
+
+
+
+
+/*********************************************************/
+
+
+
+
 //
 // // List page names.
 // var labels = ['Homepage','Projects In-Progress','Favorite Projects'];
@@ -17,73 +59,9 @@
 //
 
 
+// List public project names.
 // const projectPublicNames = [
-//
-// 	{	name:'Home',
-// 		url:'chooser'
-// 	},
-// 	{	name:'Finance',
-// 		url:'finance'
-// 	},
-// 	{	name:'Math',
-// 		url:'math'
-// 	},
-// 	{	name:'Music',
-// 		url:'music'
-// 	},
-// 	{	name:'Probability',
-// 		url:'probability'
-// 	},
-// 	{	name:'Real Estate',
-// 		url:'reast'
-// 	},
-// 	{	name:'Stocks',
-// 		url:'stocks'
-// 	}
-//
+// 	'chooser','finance', 'math', 'music', 'probability', 'stocks',
+// 	'calc','cluster','tabs','db','diagram','flex','grid','igt','levels','matrix','mementomori','notes','phonecall','regexp','sidenav','tags','tasks','waves'
 // ];
 
-
-
-/*********************************************************/
-
-
-
-// List project names.
-const projectNames = [
-	'3d','10000cards',
-	'admin','affirm','agency','animation','apple','application','aspectratio','atg','audio','autocomplete',
-	'backToTop','band','battlerap','big3dcard','blog','bodyshop','book','books','box','bs5','bubblesort','buttonpress',
-	'calc','calculator','calendar','cali','caliB','canvas','cardflipgallery3d','carnell','carnell2','carousel','carousel2','carousel3d',
-	'chadpiff','chart','chat','checkout','chooser','chooser-old','cis255','clearfix','clock','clone','cluster','cluster1d','cms',
-	'code','cognitivebias','collapsible','color','comingsoon','contact','contactform','contextmenu', 'cookie','copyright','covid','cubecarousel','cutout',
-	'dashboard','db','decision','deck','depth','device','diagram','dice','dollar','draggable',
-	'eazechain','ecomm','ecommerce','editor','enter','essay','event','evolution','ezdev',
-	'fam','family','filter','finance','fitness','flex','flexbreak','font','form','form3d','format',
-	'gallerywalk','gametapes','goodz','gradient','graph','grid','grocery','grocery2','guess',
-	'hero','hero2','hovercard',
-	'icon','igt','image','input','instagram',
-	'jay','jenna','json',
-	'keyframes','keygenius','koan','kumba',
-	'land','layouts','leads','learner','levels','lightSwitch','linktree','loading','login','logo',
-	'macbook','math','matrix','media','mementomori','mentalmodels','message','minion','music',
-	'navbar','neuro67','news','notes',
-	'overflow','overlay',
-	'pageNexter','personality','perspective','phonecall','pinyin','plax','plax2','pokeball','pong','popup','portfolio','postercircle','pricing','probability','profilecard','progressbar','prototype',
-	'rating','reast','refresh','regexp','resume','robinhood',
-	'scale3d','scroller','scrollProgressBar','scrollsnap','search','shape','sidenav','signin','skills','sky','slide','slider','slideshow','smoothscroll','smosa',
-	'socialmedia','solarsystem','sort','soundfx','spades','sphere','spinner','splash','squarespace','stocks','storage','student','superforg','svsu','syntax',
-	'tabs','tabs3d','tagboard','tags','tasks','template','theme','ticker','toast','todo','topnav','traffic','travisscott','trifold3d','trig','tropicalghana','typetest','typewriter',
-	'univ','unseenstrts','urlparams','user',
-	'vanilla','video','viewport','vidbg','vidbg2',
-	'watchthescreen','waves','wordcube','wts',
-	'ytblp',
-	'zbar'
-];
-
-
-// List public project names.
-const projectPublicNames = [
-	'chooser','finance', 'math', 'music', 'probability', 'stocks',
-	'calc','cluster','tabs','db','diagram','flex','grid','igt','levels','matrix','mementomori','notes','phonecall','regexp','sidenav','tags','tasks','waves'
-];
