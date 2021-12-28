a9869a83f5d4d3c410cd111a85c654906db75d3d
A Titus <25465781+atg973@users.noreply.github.com>
2021-12-27 21:49:33 -0500

    Add files via upload

--- video/database.js
+++ video/database.js
@@ -1,108 +1,377 @@
 
+
+// Define current user. 
+let currentuserid = 0;
+
+
+/*****/
+
+
+// Define video data. 
 let videoData = [
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:1,
+		title:'Video Title A',
+		authorid:0,
+		viewcount:1,
+		uploaddate:1640533308052,
 		vidurl:'media/0000001.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/0000001.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:12,
+		title:'Video Title B',
+		authorid:7,
+		viewcount:12,
+		uploaddate:1640533308052,
 		vidurl:'media/0000002.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/0000002.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:123,
+		title:'Video Title C',
+		authorid:1,
+		viewcount:123,
+		uploaddate:1640533308052,
 		vidurl:'media/1436812.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1436812.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:1234,
+		title:'Video Title D',
+		authorid:6,
+		viewcount:1234,
+		uploaddate:1640533308052,
 		vidurl:'media/1437396.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1437396.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:12045,
+		title:'Video Title E',
+		authorid:2,
+		viewcount:12045,
+		uploaddate:1640533308052,
 		vidurl:'media/1583096.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1583096.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:123456,
+		title:'Video Title F',
+		authorid:5,
+		viewcount:123456,
+		uploaddate:1640533308052,
 		vidurl:'media/1644693.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1644693.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:1234567,
+		title:'Video Title G',
+		authorid:3,
+		viewcount:1234567,
+		uploaddate:1640533308052,
 		vidurl:'media/1654216.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1654216.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:12345678,
+		title:'Video Title H',
+		authorid:4,
+		viewcount:12345678,
+		uploaddate:1640533308052,
 		vidurl:'media/1720219.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1720219.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:123456789,
-		vidurl:'media/1720219j.mp4',
-		thumbnailurl:'pic/xyz.jpg',
-	},
-	{
-		title:'Video Title',
-		author:'Video Author',
-		views:1234567890,
+		title:'Video Title I',
+		authorid:0,
+		viewcount:1234567890,
+		uploaddate:1640533308052,
 		vidurl:'media/1722697.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1722697.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:12345678901,
+		title:'Video Title J',
+		authorid:0,
+		viewcount:12345678901,
+		uploaddate:1640533308052,
 		vidurl:'media/1723017.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/1723017.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:123456789012,
+		title:'Video Title K',
+		authorid:0,
+		viewcount:123456789012,
+		uploaddate:1640533308052,
 		vidurl:'media/2063228.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/2063228.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:1234567890123,
+		title:'Video Title L',
+		authorid:0,
+		viewcount:1234567890123,
+		uploaddate:1640533308052,
 		vidurl:'media/2169880.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/2169880.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:12345678901234,
+		title:'Video Title M',
+		authorid:0,
+		viewcount:12345678901234,
+		uploaddate:1640533308052,
 		vidurl:'media/2231485.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/2231485.png',
 	},
 	{
-		title:'Video Title',
-		author:'Video Author',
-		views:123456789012345,
+		title:'Video Title N',
+		authorid:0,
+		viewcount:123456789012345,
+		uploaddate:1640533308052,
 		vidurl:'media/2818546.mp4',
-		thumbnailurl:'pic/xyz.jpg',
+		thumbnailurl:'pic/2818546.png',
+	},
+];
+
+
+// Define customer account data. 
+const userdata = [
+	{
+		name:'UserName A',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
+	},
+	{
+		name:'UserName B',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
+	},
+	{
+		name:'UserName C',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
+	},
+	{
+		name:'UserName D',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
+	},
+	{
+		name:'UserName E',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
+	},
+	{
+		name:'UserName F',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
+	},
+	{
+		name:'UserName G',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
+	},
+	{
+		name:'UserName H',
+		email:'UserName@mail.com',
+		photourl:'pic/avatar-m.png',
+		
+		// Define initial list of liked videos. 
+		likedIds:[ 8, 2, 5, 7 ],
+		
+		// Define initial list of disliked videos. 
+		dislikedIds:[ 3, 4 ],
+		
+		// Define initial list of downloaded videos. 
+		downloadedIds:[ 8, 5, 2 ],
+		
+		// Define initial list of downloaded videos. 
+		savedIds:[ 8, 5, 13, 2 ],
+		
+		// Define initial list of playlists. 
+		playlists:[
+			{
+				plistname:'Playlist Name',
+				vidIds:[ 2, 5, 3, 6, 7, 1 ]
+			}
+		],
+		
+		// Define initial subscriptions. 
+		subscriptions:[ 7, 5, 1, 2, 0 ],
+		
+		// Define initial subscriber count. 
+		subscribercount:6789,
 	},
 ];
--- video/video.js
+++ video/video.js
@@ -1,10 +1,97 @@
 
-
-// Load video links. 
-// loadVideoLinks();
+// Get time value of current moment. 
+let now = new Date().valueOf();
+// console.log('now:',now);
 
 
 /*****/
+
+
+// Load video on page. 
+loadVideoById(0);
+
+// Load video links. 
+loadVideoLinks();
+
+// Activate reaction buttons. 
+activateReactBtns();
+
+
+/*****/
+
+
+// Load video on page (given video id). 
+function loadVideoById(vidid) {
+	console.log('Loading video... id:',vidid);
+
+	// Get video data. 
+	let vidsrc = videoData[vidid];
+
+	// Get video author data. 
+	let author = userdata[vidsrc.authorid];
+
+
+	// Load video. 
+	document.querySelector('main#main main.player div.vid').innerHTML = `
+	<!-- video -->
+	<video src="${ vidsrc.vidurl }" autoplay muted controls></video>
+	<!-- /video -->`;
+
+
+	// Load video title. 
+	document.getElementById('title').innerHTML = vidsrc.title;
+	
+	// Load video view count. 
+	document.getElementById('viewcount').innerHTML = formatViewCount(vidsrc.viewcount,true);
+
+	
+	// Load video reaction: liked. 
+	let liked = userdata[currentuserid].likedIds.includes(vidid);
+	if(liked) document.getElementById('likebtn').classList.add('active');
+	else document.getElementById('likebtn').classList.remove('active');
+	// console.log('liked:',liked);
+
+	// Load video reaction: disliked. 
+	let disliked = userdata[currentuserid].dislikedIds.includes(vidid);
+	if(disliked) document.getElementById('dislikebtn').classList.add('active');
+	else document.getElementById('dislikebtn').classList.remove('active');
+	// console.log('disliked:',disliked);
+	
+	// Load video reaction: downloaded / not downloaded. 
+	let downloaded = userdata[currentuserid].downloadedIds.includes(vidid);
+	if(downloaded) document.getElementById('downloadbtn').classList.add('active');
+	else document.getElementById('downloadbtn').classList.remove('active');
+	
+	// Load video reaction: saved / not saved. 
+	let saved = userdata[currentuserid].savedIds.includes(vidid);
+	if(saved) document.getElementById('savebtn').classList.add('active');
+	else document.getElementById('savebtn').classList.remove('active');
+
+	
+	// Load video author. 
+	document.getElementById('channelname').innerHTML = author.name;
+	document.getElementById('channelsubcount').innerHTML = formatSubCount(author.subscribercount);
+	document.getElementById('avatar').style.backgroundImage = `url('${author.photourl}')`;
+
+	// Load subscriber button status. 
+	let subscribed = userdata[currentuserid].subscriptions.includes(vidsrc.authorid);
+	if(subscribed) document.getElementById('subbtn').classList.add('active');
+	else document.getElementById('subbtn').classList.remove('active');
+
+
+	// TODO: Load status of notification bell button. 
+	// let notifbtn = document.getElementById('notifbtn');
+	// let notifs = 2;
+	// if(notifs>=2) {
+	// 	notifbtn.classList.add('all');
+	// 	// notifbtn.classList.add('on','all');
+	// }
+	// if(notifs==1) {
+	// 	notifbtn.classList.remove('all');
+	// 	notifbtn.classList.add('on');
+	// }
+	// else notifbtn.classList.remove('all','on');
+}
 
 
 // Load video links. 
@@ -14,37 +101,78 @@ function loadVideoLinks() {
 	let result = '';
 
 	// Add all data to result. 
-	for(let vid of videoData) {
+	for(let vidIndex in videoData) {
+
+		// Get video object. 
+		let vid = videoData[vidIndex];
+
+		// Construct video item. 
 		result += `
 		<!-- viditem -->
 		<li class="viditem">
+
 			<!-- vidlink -->
-			<a class="vidlink" href="javascript:void(0)" data-src="${vid.vidurl}">
+			<a class="vidlink" href="javascript:void(0)" data-id="${ vidIndex }">
 
 				<!-- photo -->
-				<div class="photo" style="background-image:url('${vid.thumbnailurl}');"></div>
+				<div class="photo" style="background-image:url('${ vid.thumbnailurl }');"></div>
 				<!-- /photo -->
 
 				<!-- content -->
 				<div class="content">
 
-					<!-- title -->
-					<span class="title">${vid.title}</span>
-					<!-- /title -->
+					<!-- avatar -->
+					<div class="avatar"></div>
+					<!-- /avatar -->
 
-					<!-- author -->
-					<span class="author">${vid.author}</span>
-					<!-- /author -->
+					<!-- meta -->
+					<div class="meta">
 
-					<!-- details -->
-					<div class="details">${formatViewCount(vid.views)} &bull; ${formatViewCount(vid.views)}</div>
-					<!-- /details -->
+						<!-- vidtitle -->
+						<div class="vidtitle">${ vid.title }</div>
+						<!-- /vidtitle -->
+
+						<!-- metadata -->
+						<div class="metadata">
+
+							<!-- vidauthor -->
+							<span class="vidauthor">${ userdata[vid.authorid].name }</span>
+							<!-- /vidauthor -->
+
+							<!-- dot -->
+							<span class="dot a">&sdot;</span>
+							<!-- /dot -->
+
+							<!-- viddata -->
+							<div class="viddata">
+
+								<!-- viewcount -->
+								<span class="viewcount">${ formatViewCount(vid.viewcount) }</span>
+								<!-- /viewcount -->
+
+								<!-- dot -->
+								<span class="dot b">&sdot;</span>
+								<!-- /dot -->
+
+								<!-- timesince -->
+								<span class="timesince">${ formatTimeAgo(vid.uploaddate) }</span>
+								<!-- /timesince -->
+
+							</div>
+							<!-- /viddata -->
+							
+						</div>
+						<!-- /metadata -->
+						
+					</div>
+					<!-- /meta -->
 					
 				</div>
 				<!-- /content -->
 
 			</a>
 			<!-- /vidlink -->
+
 		</li>
 		<!-- /viditem -->`;
 	}
@@ -62,69 +190,62 @@ function loadVideoLinks() {
 	for(let vidlink of allvidlinks ) {
 		vidlink.addEventListener('click',selectVideo);
 	}
-}
-
-// Select video. 
-function selectVideo(event) {
-
-	// Highlight video. 
-	highlightVideo();
-
-	// Load selected video on page. 
-	loadVideo();
 
 	/*****/
-	
-	// Highlight video. 
-	function highlightVideo() {
 
-		// Get all vid links in playlist. 
-		let allviditems = document.querySelectorAll('aside.playlist ul.vidlist li.viditem');
+	// Select video. 
+	function selectVideo(event) {
 
-		// Un-highlight all videos in playlist. 
-		for(let viditem of allviditems) {
-			viditem.classList.remove('active');
+		// Highlight video. 
+		highlightVideo();
+
+		// Load selected video on page. 
+		let id = event.currentTarget.getAttribute('data-id');
+		loadVideoById(id);
+
+		/*****/
+		
+		// Highlight video. 
+		function highlightVideo() {
+
+			// Get all vid links in playlist. 
+			let allviditems = document.querySelectorAll('aside.playlist ul.vidlist li.viditem');
+
+			// Un-highlight all videos in playlist. 
+			for(let viditem of allviditems) {
+				viditem.classList.remove('active');
+			}
+
+			// Highlight selected video in playlist. 
+			let selectedvidlink = event.currentTarget;
+			let selectedviditem = selectedvidlink.parentElement;
+			selectedviditem.classList.add('active');
 		}
-
-		// Highlight selected video in playlist. 
-		let selectedvidlink = event.currentTarget;
-		let selectedviditem = selectedvidlink.parentElement;
-		selectedviditem.classList.add('active');
-	}
-
-	// Load selected video on page. 
-	function loadVideo() {
-
-		// Get video source. 
-		let vidsrc = event.currentTarget.getAttribute('data-src');
-
-		// Apply video source. 
-		document.querySelector('main#main main.player div.video').innerHTML = `
-		<!-- video -->
-		<video src="${vidsrc}" autoplay muted controls></video>
-		<!-- /video -->`;
 	}
 }
 
-// Format view count number. 
-function formatViewCount(num) {
+
+// Format subscriber count number. 
+function formatSubCount(num) {
 	
 	// Define components of formatted number. 
 	let coeff = 1*num;
 	let suffindex = 0;
-	let suffix = [' views','K views','M views','B views','T views',' view'];	// potential suffixes
+	let suffix = [' subscribers','K subscribers','M subscribers','B subscribers','T subscribers',' subscriber'];	// potential suffixes
 
 	// Divide number until less than a thousand. 
 	while(coeff>1000 && suffindex<5) {
 
 		// Divide number by thousand. 
 		coeff /= 1000;
+		// console.log('\tcoeff',coeff);
 
 		// Increment suffix index. 
 		suffindex++;
+		// console.log('\tsuffindex',suffindex);
 	}
 
-	// Add final formatting to view count. 
+	// Add final formatting to subscriber count. 
 	coeff = coeff.toFixed(1);
 	let removeExtraZero = coeff.includes('.0');
 	if(removeExtraZero) coeff = coeff.substr(0,coeff.length-2);
@@ -134,12 +255,184 @@ function formatViewCount(num) {
 	return ( coeff + suffix[suffindex] );
 }
 
-// TODO: Format time ago number. 
-function formatTimeAgo(ets) {
+
+// Format view count number. 
+function formatViewCount(num,expanded=false) {
+	// console.log('num',num, (expanded)?('expanded'):('abbreviated') );
+	
+	// Format general integer. 
+	if(expanded) {
+
+		// Create string representation of number. 
+		let numstr = '' + Math.floor(num);
+		// console.log('numstr:',numstr);
+
+		// Add commas to numbers longer than 3 digits. 
+		if(numstr.length>3) {
+
+			// Split number into pieces. 
+			let splitNumber = numstr.split('');
+			let newSplitNumber = [];
+			// console.log('splitNumber:',splitNumber);
+			// console.log('newSplitNumber:',newSplitNumber);
+	
+
+			// Get bottom digit group and add comma. Repeat till last digit group. 
+			while(splitNumber.length>3) {
+				newSplitNumber.unshift( splitNumber.pop() );
+				newSplitNumber.unshift( splitNumber.pop() );
+				newSplitNumber.unshift( splitNumber.pop() );
+				newSplitNumber.unshift(',');
+			}
+			// console.log('splitNumber:',splitNumber);
+			// console.log('newSplitNumber:',newSplitNumber);
+
+			// Get last digit group. 
+			while(splitNumber.length) {
+				newSplitNumber.unshift( splitNumber.pop() );
+			}
+			// console.log('splitNumber:',splitNumber);
+			// console.log('newSplitNumber:',newSplitNumber);
+	
+			// Reassemble split number into string. 
+			numstr = newSplitNumber.join('');
+		}
+
+		// Return final result text. 
+		if(numstr==1) return ( numstr + ' view' );
+		else return ( numstr + ' views' );
+	}
+
+	// Format abbreviated number. 
+	else {
+	
+		// Define components of formatted number. 
+		let coeff = 1*num;
+		let suffindex = 0;
+		let suffix = [' views','K views','M views','B views','T views',' view'];	// potential suffixes
+	
+		// Divide number until less than a thousand. 
+		while(coeff>1000 && suffindex<5) {
+	
+			// Divide number by thousand. 
+			coeff /= 1000;
+			// console.log('\tcoeff',coeff);
+	
+			// Increment suffix index. 
+			suffindex++;
+			// console.log('\tsuffindex',suffindex);
+		}
+	
+		// Add final formatting to view count. 
+		coeff = coeff.toFixed(1);
+		let removeExtraZero = coeff.includes('.0');
+		if(removeExtraZero) coeff = coeff.substr(0,coeff.length-2);
+		if(coeff==1 && suffindex==0) suffindex = 5;
+	
+		// Return final result text. 
+		return ( coeff + suffix[suffindex] );
+
+	}
+}
+
+
+// Format 'time since upload' number. 
+function formatTimeAgo(uploaddate) {
+
+	// Get estimated time since upload date (ETS). 
+	let dt = now - 1*uploaddate;
+	// console.log('dt:',dt);
+
+	// Treat non-positive ets numbers as right now. 
+	if(dt<=0) return 'now';
 
 	// Define components of formatted number. 
-	let coeff = 1*ets;
+	let coeff = dt;	// /1000/60/60/24/365
 	let suffindex = 0;
-	let suffix = [' second','minute','hour','day','week','month','year'];	// potential suffixes
+	let suffix = [' millisecond',' second',' minute',' hour',' day',' week',' month',' year'];	// potential suffixes
+	let limits = [1000, 60, 60, 24, 7, 4.3, 12, 1000];
+
+	// Divide number until... 
+	while(coeff>limits[suffindex]) {
+
+		// Divide coefficient. 
+		coeff /= limits[suffindex];
+		// console.log('\tcoeff',coeff);
+
+		// Increment suffix index. 
+		suffindex++;
+		// console.log('\tsuffindex',suffindex);
+	}
+
+	// Add final formatting to view count. 
+	coeff = coeff.toFixed(0);
+	// let removeExtraZero = coeff.includes('.0');
+	// if(removeExtraZero) coeff = coeff.substr(0,coeff.length-2);
+
+	return (coeff + suffix[suffindex]+((coeff>1)?('s ago'):(' ago')) );
+}
+
+
+// Activate reaction buttons. 
+function activateReactBtns() {
+
+	// Get all reaction buttons. 
+	// let allreactbtns = document.querySelectorAll('main.player div.reaction div.btn');
+
+	// Attach function to all reaction buttons. 
+	// for(let btn of allreactbtns) {
+	// 	btn.addEventListener('click',reactToVideo);
+	// }
+
+	// Activate like and dislike buttons. 
+	let likebtn = document.getElementById('likebtn');
+	likebtn.addEventListener('click',likeVideo);
+	let dislikebtn = document.getElementById('dislikebtn');
+	dislikebtn.addEventListener('click',dislikeVideo);
+
+	// TODO: Activate share button. 
+	let sharebtn = document.getElementById('sharebtn');
+	sharebtn.addEventListener('click',reactToVideo);
+
+	// TODO: Activate download button. 
+	let downloadbtn = document.getElementById('downloadbtn');
+	downloadbtn.addEventListener('click',reactToVideo);
+
+	// TODO: Activate save button. 
+	let savebtn = document.getElementById('savebtn');
+	savebtn.addEventListener('click',reactToVideo);
+
+	/*****/
+
+	// TODO: React to video. 
+	function reactToVideo(event) {
+		// console.log('Reacting to video with btn:', event.currentTarget);
+		let btn = event.currentTarget;
+		btn.classList.toggle('active');
+	}
+
+	// Add 'like' to video. 
+	function likeVideo(event) {
+
+		// De-activate 'dislike' button. 
+		dislikebtn.classList.remove('active');
+
+		// Activate 'like' button. 
+		likebtn.classList.add('active');
+
+		// TODO: Save reaction to database. 
+	}
+
+	// Add 'dislike' to video. 
+	function dislikeVideo(event) {
+
+		// De-activate 'like' button. 
+		likebtn.classList.remove('active');
+
+		// Activate 'dislike' button. 
+		dislikebtn.classList.add('active');
+
+		// TODO: Save reaction to database. 
+	}
 }
 
