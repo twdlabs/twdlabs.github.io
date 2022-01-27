ba013753c5730ce52b23972690a7566ff4149b3a
A Titus <25465781+atg973@users.noreply.github.com>
2022-01-27 05:53:57 -0500

    Add files via upload

---
+++ retool/database.js
@@ -0,0 +1,131 @@
+
+let datasources = [
+	
+	[
+		{
+			name:'Google Sheets',
+			imageurl:'images/google-sheets-mini.svg'
+		},
+		{
+			name:'PostgreSQL',
+			imageurl:'images/postgresql.svg'
+		},
+		{
+			name:'Stripe',
+			imageurl:'images/stripe.svg'
+		},
+		{
+			name:'Elasticsearch',
+			imageurl:'images/elasticsearch.svg'
+		},
+		{
+			name:'MongoDB',
+			imageurl:'images/mongodb-mini.svg'
+		},
+		{
+			name:'MySQL',
+			imageurl:'images/mysql-mini.svg'
+		},
+	],
+	
+	[
+		{
+			name:'BigQuery',
+			imageurl:'images/bigquery-mini.svg'
+		},
+		{
+			name:'Snowflake',
+			imageurl:'images/snowflake.svg'
+		},
+		{
+			name:'Denodo',
+			imageurl:'images/denodo.svg'
+		},
+		{
+			name:'Vertica',
+			imageurl:'images/vertica.svg'
+		},
+		{
+			name:'AWS DynamoDB',
+			imageurl:'images/aws-dynamodb.svg'
+		},
+		{
+			name:'Redis',
+			imageurl:'images/redis.svg'
+		},
+	],
+	
+	[
+		{
+			name:'AWS Redshift',
+			imageurl:'images/aws-redshift.svg'
+		},
+		{
+			name:'Firebase',
+			imageurl:'images/firebase.svg'
+		},
+		{
+			name:'AWS Lambda',
+			imageurl:'images/aws-lambda.svg'
+		},
+		{
+			name:'Twilio',
+			imageurl:'images/twilio.svg'
+		},
+		{
+			name:'GraphQL',
+			imageurl:'images/graphql.svg'
+		},
+		{
+			name:'AWS S3',
+			imageurl:'images/aws-s3.svg'
+		},
+	],
+	
+	[
+		{
+			name:'Salesforce',
+			imageurl:'images/salesforce-mini.svg'
+		},
+		{
+			name:'Basecamp',
+			imageurl:'images/basecamp.svg'
+		},
+		{
+			name:'Slack',
+			imageurl:'images/slack.svg'
+		},
+		{
+			name:'Cassandra',
+			imageurl:'images/cassandra.svg'
+		},
+		{
+			name:'Sendgrid',
+			imageurl:'images/sendgrid.svg'
+		},
+		{
+			name:'Github',
+			imageurl:'images/github.svg'
+		},
+	],
+	
+	[
+		{
+			name:'Google Cloud Storage',
+			imageurl:'images/google-cloud-storage-mini.svg'
+		},
+		{
+			name:'Close.io',
+			imageurl:'images/close-io.svg'
+		},
+		{
+			name:'MS SQL Server',
+			imageurl:'images/microsoft-sql-server-mini.svg'
+		},
+		{
+			name:'AWS Athena',
+			imageurl:'images/aws-athena.svg'
+		},
+	],
+	
+];
