c670d7da44935f8cb362d1a4bfc33d083bab5376
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-02-14 10:07:45 -0500

    Add files via upload

---
+++ testimonials/database.js
@@ -0,0 +1,225 @@
+
+
+// Initialize user data. 
+const userData = [
+	{
+		fname:'Ana',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Bill',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Chris',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'David',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Eli',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Fred',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'George',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Hilary',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Ian',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Jane',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Jay',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Jenny',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Jill',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Jim',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Joe',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'John',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Kate',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Leah',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Mike',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Nina',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Oscar',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Peter',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Quinten',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Rachel',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Steve',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Tim',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Tori',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Ursula',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Victor',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Wendy',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+	{
+		fname:'Xavier',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-m.png',
+	},
+	{
+		fname:'Yolanda',
+		lname:'Doe',
+		position:'Marketing Web Developer',
+		avatarurl:'assets/images/avatar-f.png',
+	},
+];
+
+// Initialize testimonial data. 
+const testimonialData = [
+	{
+		clientid:1,
+		testimonialcontents:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dicta eveniet dolorem. Assumenda sapiente maiores alias minus illo quos nobis. Aut quibusdam qui provident quis aperiam eaque et quia assumenda! Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+	},
+	{
+		clientid:10,
+		testimonialcontents:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dicta eveniet dolorem. Assumenda sapiente maiores alias minus illo quos nobis. Aut quibusdam qui provident quis aperiam eaque et quia assumenda! Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+	},
+	{
+		clientid:4,
+		testimonialcontents:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dicta eveniet dolorem. Assumenda sapiente maiores alias minus illo quos nobis. Aut quibusdam qui provident quis aperiam eaque et quia assumenda! Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+	},
+	{
+		clientid:12,
+		testimonialcontents:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dicta eveniet dolorem. Assumenda sapiente maiores alias minus illo quos nobis. Aut quibusdam qui provident quis aperiam eaque et quia assumenda! Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+	},
+	{
+		clientid:14,
+		testimonialcontents:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dicta eveniet dolorem. Assumenda sapiente maiores alias minus illo quos nobis. Aut quibusdam qui provident quis aperiam eaque et quia assumenda! Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+	},
+	{
+		clientid:31,
+		testimonialcontents:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dicta eveniet dolorem. Assumenda sapiente maiores alias minus illo quos nobis. Aut quibusdam qui provident quis aperiam eaque et quia assumenda! Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+	},
+];
---
+++ testimonials/testimonials.js
@@ -0,0 +1,92 @@
+
+
+// Initialize index of current testimonial. 
+let currentIndex = 0;
+
+// Fill data. 
+fillData();
+
+
+/*****/
+
+
+// Fill data. 
+function fillData() {
+
+	// Show testimonials from database. 
+	let result = '';
+	for(let item of testimonialData) {
+		result += createTestimonial(item);
+	}
+	result += createTestimonial(testimonialData[0]);
+	document.querySelector('div.contentbox div.inner').innerHTML = result;
+
+	/*****/
+
+	function createTestimonial(testimonialitem) {
+		
+		// Get client's data. 
+		let client = userData[testimonialitem.clientid];
+		return `
+		<!-- item -->
+		<div class="item">
+	
+			<!-- head -->
+			<div class="head">
+	
+				<!-- avatar -->
+				<div class="avatar">${ (client.avatarurl) ? (`<img src="../chat/${client.avatarurl}" alt="">`) : ('') }</div>
+				<!-- /avatar -->
+	
+				<!-- remainder -->
+				<div class="remainder">
+	
+					<!-- name -->
+					<h2 class="name">${client.fname} ${client.lname}</h2>
+					<!-- /name -->
+	
+					<!-- position -->
+					<div class="position">${client.position}</div>
+					<!-- /position -->
+	
+				</div>
+				<!-- /remainder -->
+	
+			</div>
+			<!-- /head -->
+	
+			<!-- content -->
+			<div class="content">
+	
+				<!-- textcopy -->
+				<p class="textcopy">${testimonialitem.testimonialcontents}</p>
+				<!-- /textcopy -->
+	
+			</div>
+			<!-- /content -->
+	
+		</div>
+		<!-- /item -->`;
+	}
+}
+
+// Move to adjacent testimonial. 
+function deltaIndex(n) {
+
+	// Increment current index. 
+	currentIndex += n;
+
+	// Create circularity of testimonial data. 
+	if(currentIndex>=testimonialData.length) {
+		currentIndex = 0;
+	}
+	else if(currentIndex<0) {
+		currentIndex = testimonialData.length - 1;
+	}
+
+	// Display appropriate testimonial. 
+	let dy = (currentIndex) ? (-100*currentIndex) : (-100*testimonialData.length);
+	document.querySelector('div.contentbox div.inner').style.transform = `translateY(${dy}%)`;
+
+	console.log('currentIndex:',currentIndex);
+}
