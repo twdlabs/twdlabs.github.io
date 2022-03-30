e45e70b6967369770adfb8bfe215b39415883d22
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-03-30 00:27:22 -0400

    Add files via upload

--- admin2/admin.js
+++ admin2/admin.js
@@ -5,41 +5,49 @@ let navdata = [
 	{
 		name:'Dashboard',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>'
 	},
 	{
 		name:'User',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>'
 	},
 	{
 		name:'Messages',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>'
 	},
 	{
 		name:'Analytics',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="M7.5 1.018a7 7 0 0 0-4.79 11.566L7.5 7.793V1.018zm1 0V7.5h6.482A7.001 7.001 0 0 0 8.5 1.018zM14.982 8.5H8.207l-4.79 4.79A7 7 0 0 0 14.982 8.5zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>'
 	},
 	{
 		name:'File Manager',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z"/>'
 	},
 	{
 		name:'Orders',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>'
 	},
 	{
 		name:'Saved',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>'
 	},
 	{
 		name:'Settings',
 		link:'javascript:void(0)',
+		count:10,
 		innersvg:'<path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/><path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>'
 	}
 ];
@@ -48,7 +56,7 @@ let navdata = [
 
 // Add navigation items. 
 addNavItems();
-let autoToggle = setInterval(toggleSideNav,2500);
+// let autoToggle = setInterval(toggleSideNav,2500);
 
 
 
@@ -60,32 +68,67 @@ function addNavItems() {
 
 	// Create navigation items to list. 
 	let result = '';
-	for(let i=0 ; i<navdata.length ; i++) {
-		// Get item. 
-		let item = navdata[i];
-	
+	for(let item of navdata) {
+		
 		// Define item. 
-		result += '<!-- navitem -->';
-		result += '<li class="navitem">';
-			result += '<!-- navlink -->';
-			result += `<a href="${item.link}" class="navlink">`;
-				result += '<!-- icon -->';
-				result += `<svg class="icon" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">${item.innersvg}</svg>`;
-				result += '<!-- /icon -->';
-				result += '<!-- caption -->';
-				result += `<span class="caption">${item.name}</span>`;
-				result += '<!-- /caption -->';
-				result += '<!-- tooltip -->';
-				result += `<span class="tooltip">${item.name}</span>`;
-				result += '<!-- /tooltip -->';
-			result += '</a>';
-			result += '<!-- /navlink -->';
-		result += '</li>';
-		result += '<!-- /navitem -->';
+		result += `
+		<!-- navitem -->
+		<li class="navitem">
+			<!-- navlink -->
+			<a class="navlink" href="${item.link}">`;
+		result += `
+				<!-- icon -->
+				<svg class="icon" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+					${item.innersvg}
+				</svg>
+				<!-- /icon -->`;
+		result += `
+				<!-- caption -->
+				<span class="caption">${item.name}</span>
+				<!-- /caption -->`;
+		result += `
+				<!-- badge -->
+				<span class="badge">${item.count}</span>
+				<!-- /badge -->`;
+		result += `
+				<!-- tooltip -->
+				<span class="tooltip">${item.name}</span>
+				<!-- /tooltip -->`;
+		result += `
+			</a>
+			<!-- /navlink -->
+		</li>
+		<!-- /navitem -->`;
+		result += ``;
+		result += ``;
+
+		// result += '<!-- navitem -->';
+		// result += '<li class="navitem">';
+
+		// 	result += '<!-- navlink -->';
+		// 	result += `<a class="navlink" href="${item.link}">`;
+
+		// 		result += '<!-- icon -->';
+		// 		result += `<svg class="icon" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">${item.innersvg}</svg>`;
+		// 		result += '<!-- /icon -->';
+				
+		// 		result += '<!-- caption -->';
+		// 		result += `<span class="caption">${item.name}</span>`;
+		// 		result += '<!-- /caption -->';
+
+		// 		result += '<!-- tooltip -->';
+		// 		result += `<span class="tooltip">${item.name}</span>`;
+		// 		result += '<!-- /tooltip -->';
+
+		// 	result += '</a>';
+		// 	result += '<!-- /navlink -->';
+
+		// result += '</li>';
+		// result += '<!-- /navitem -->';
 	}
 	
 	// Add navigation items to list. 
-	let navlist = document.querySelector('ul#navlist');
+	let navlist = document.querySelector('nav#sidebar ul#navlist');
 	console.log('navlist',navlist);
 	navlist.innerHTML = result;
 }
