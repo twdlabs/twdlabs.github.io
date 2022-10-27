d0b2bd9f3b2d6612e02e06bcd7e531dc60446be9
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-10-27 08:50:34 -0400

    Add files via upload

--- rolodex/rolodex.js
+++ rolodex/rolodex.js
@@ -1,38 +1,90 @@
 
-let result = '';
-for(let i in teamdata) {
 
-	// Get data for given team member. 
-	let employee = teamdata[i];
+// Get load destination. 
+const destination = document.querySelector('main.main');
+
+
+/*****/
+
+
+// Add cards to page. 
+loadCards();
+
+
+/*****/
+
+
+// Add cards to page. 
+function loadCards() {
+
+	// Initialize result. 
+	let result = '';
+
+	// Go thru all team members. 
+	for(let i in teamdata) {
 	
-	// Create card for given team member. 
-	result += `
-	<!-- card -->
-	<div class="card" style="--delay:${i}">
-
-		<!-- avatar -->
-		<img class="avatar" src="${ employee.avatarurl }" alt="avatar">
-		<!-- /avatar -->
-
-		<!-- details -->
-		<div class="details">
-
-			<!-- name -->
-			<div class="name">${ employee.name }</div>
-			<!-- /name -->
-
-			<!-- title -->
-			<div class="title">${ employee.job }</div>
-			<!-- /title -->
-
+		// Get data for given team member. 
+		let employee = teamdata[i];
+		
+		// Create card for given team member. 
+		result += `
+		<!-- card -->
+		<div class="card" style="--i:${ -1*i }">
+	
+			<!-- avatar -->
+			<img class="avatar" src="${ employee.avatarurl }" alt="avatar">
+			<!-- /avatar -->
+	
+			<!-- details -->
+			<div class="details">
+	
+				<!-- name -->
+				<div class="name">${ employee.name }</div>
+				<!-- /name -->
+	
+				<!-- title -->
+				<div class="title">${ employee.job }</div>
+				<!-- /title -->
+	
+			</div>
+			<!-- /details -->
+	
+			<!-- followbtn -->
+			<a class="followbtn" href="javascript:void(0)">Follow</a>
+			<!-- /followbtn -->
+	
 		</div>
-		<!-- /details -->
+		<!-- /card -->`;
+	}
 
-		<!-- followbtn -->
-		<a class="followbtn" href="javascript:void(0)">Follow</a>
-		<!-- /followbtn -->
+	// Add result to page. 
+	destination.innerHTML = result;
 
-	</div>
-	<!-- /card -->`;
+	// Activate card clicks. 
+	activateCards();
+	
+	/****/
+
+	// Activate card clicks. 
+	function activateCards() {
+	
+		// Get all loaded cards. 
+		const allCards = document.querySelectorAll('div#container main.main div.card');
+	
+		// Go thru all cards. 
+		for(let card of allCards) {
+			// Activate given card. 
+			card.addEventListener('click',showCardData)
+		}
+	
+		/***/
+	
+		// Show card data. 
+		function showCardData(event) {
+	
+			// Get selected card. 
+			let selectedCard = event.currentTarget;
+			console.log('Selected card:',selectedCard.innerText);
+		}
+	}
 }
-document.querySelector('main.main').innerHTML = result;
