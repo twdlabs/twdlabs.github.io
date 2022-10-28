75a79a8fd6e60d70a394daa4636b5eb350a7a07d
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-10-28 01:52:59 -0400

    Add files via upload

---
+++ univ/assets/database/programdatabase.js
@@ -0,0 +1,66 @@
+
+
+
+// Define program data. 
+const programData = [
+	// {
+	// 	deptid:'XYZ',
+	// 	programid:'XYZ',
+	// 	programname:'Xyz Department',
+	// },
+	{
+		deptid:'CSIS',
+		programid:'CS',
+		programname:'Computer Science',
+	},
+	{
+		deptid:'CSIS',
+		programid:'CIS',
+		programname:'Computer Information Systems',
+	},
+	{
+		deptid:'CSIS',
+		programid:'COMM',
+		programname:'Communications',
+	},
+	{
+		deptid:'ENGL',
+		programid:'ENGL',
+		programname:'English',
+	},
+	{
+		deptid:'MATH',
+		programid:'MATH',
+		programname:'Mathematical Sciences',
+	},
+	{
+		deptid:'PHIL',
+		programid:'PHIL',
+		programname:'Philosophy',
+	},
+	{
+		deptid:'RPW',
+		programid:'RPW',
+		programname:'Rhetoric &amp; Professional Writing',
+	},
+	{
+		deptid:'PSYC',
+		programid:'PSYC',
+		programname:'Psychology',
+	},
+	{
+		deptid:'BL',
+		programid:'BL',
+		programname:'Xyz Department',
+	},
+	{
+		deptid:'ACCT',
+		programid:'ACCT',
+		programname:'Xyz Department',
+	},
+	{
+		deptid:'FIN',
+		programid:'FIN',
+		programname:'Xyz Department',
+	},
+];
---
+++ univ/assets/database/resultdatabase.js
@@ -0,0 +1,34 @@
+
+
+// Define default search results, the basis of final search results. 
+const defaultResults = [
+	{
+		setname:'Set Name',
+		setlist: [
+			{
+				name:'XyzResult',
+				link:'javascript:void(0)'
+			},
+			{
+				name:'XyzResult',
+				link:'javascript:void(0)'
+			},
+			{
+				name:'XyzResult',
+				link:'javascript:void(0)'
+			},
+			{
+				name:'XyzResult',
+				link:'javascript:void(0)'
+			},
+			{
+				name:'XyzResult',
+				link:'javascript:void(0)'
+			},
+			{
+				name:'XyzResult',
+				link:'javascript:void(0)'
+			},
+		]
+	},
+];
