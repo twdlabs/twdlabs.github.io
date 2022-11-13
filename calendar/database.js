8a96c4617e9f2567dd75bed93028dc0308d8a17a
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-11-12 20:58:43 -0500

    Add files via upload

---
+++ calendar/database.js
@@ -0,0 +1,129 @@
+
+
+
+// 
+const monthlength = [ 31,28,31, 30,31,30, 31,31,30, 31,30,31, ];
+
+// Define month names. 
+const monthname = [ 
+	'Jan','Feb','Mar', 
+	'Apr','May','Jun', 
+	'Jul','Aug','Sep', 
+	'Oct','Nov','Dec', 
+];
+
+// Define month full names. 
+const monthfullname = [ 
+	'January','February','March', 
+	'April','May','June', 
+	'July','August','September', 
+	'October','November','December', 
+];
+
+// 
+const months = [
+
+	{
+		name:'Jan',
+		length:31,
+		fullname:'January',
+	},
+	{
+		name:'Feb',
+		length:28,
+		fullname:'February',
+	},
+	{
+		name:'Mar',
+		length:31,
+		fullname:'March',
+	},
+
+	{
+		name:'Apr',
+		length:30,
+		fullname:'April',
+	},
+	{
+		name:'May',
+		length:31,
+		fullname:'May',
+	},
+	{
+		name:'Jun',
+		length:30,
+		fullname:'June',
+	},
+
+	{
+		name:'Jul',
+		length:31,
+		fullname:'July',
+	},
+	{
+		name:'Aug',
+		length:31,
+		fullname:'August',
+	},
+	{
+		name:'Sep',
+		length:30,
+		fullname:'September',
+	},
+
+	{
+		name:'Oct',
+		length:31,
+		fullname:'October',
+	},
+	{
+		name:'Nov',
+		length:30,
+		fullname:'November',
+	},
+	{
+		name:'Dec',
+		length:31,
+		fullname:'December',
+	},
+
+];
+
+
+
+// 
+const dayname = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat',];
+// 
+const dayfullname = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',];
+
+// 
+const daysofweek = [
+	{
+		name:'Sun',
+		fullname:'Sunday',
+	},
+	{
+		name:'Mon',
+		fullname:'Monday',
+	},
+	{
+		name:'Tue',
+		fullname:'Tuesday',
+	},
+	{
+		name:'Wed',
+		fullname:'Wednesday',
+	},
+	{
+		name:'Thu',
+		fullname:'Thursday',
+	},
+	{
+		name:'Fri',
+		fullname:'Friday',
+	},
+	{
+		name:'Sat',
+		fullname:'Saturday',
+	},
+];
