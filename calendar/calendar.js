f6ed5332eec160d1ed07eff164c0bc92b6f2e118
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-11-14 09:12:40 -0500

    Add files via upload

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,6 +1,13 @@
 // 'use strict';
 
 
+
+// Get month selector window. 
+const selector = document.querySelector('div#container main.calendar div.selector');
+const selectorbody = document.querySelector('div#container main.calendar div.selector ul.select');
+const selectorhead = document.querySelector('div#container main.calendar div.selector div.head h1.year');
+let selectorCurrentYear;
+
 // Create new date item for current date time. 
 const now = new Date();
 // console.log('Now:',now);
@@ -28,8 +35,6 @@ let currentYear = todayYear;
 let currentMonthIndex = todayMonthIndex;
 // console.log('currentMonthIndex:',currentMonthIndex);
 
-console.log( 'Current month:', currentYear, monthfullname[currentMonthIndex] );
-
 
 /*****/
 
@@ -43,23 +48,6 @@ activateShortcuts();
 
 /*****/
 
-
-// Activate keyboard shortcuts. 
-function activateShortcuts() {
-
-	// 
-	document.addEventListener('keyup',checkForShortcutKey);
-
-	/****/
-
-	// Check for shortcut key. 
-	function checkForShortcutKey(event) {
-		// 
-		console.log(event);
-		if(event.keyCode==37 || event.key=='ArrowLeft') goToPrevMonth();
-		if(event.keyCode==39 || event.key=='ArrowRight') goToNextMonth();
-	}
-}
 
 // Update calendar. 
 function updateCalendar() {
@@ -76,9 +64,10 @@ function updateCalendar() {
 
 	// Update calendar head. 
 	function updateCalendarHead() {
+		console.log( 'Current month:', currentYear, monthfullname[currentMonthIndex] );
 		
 		// Get destination. 
-		const destination = document.querySelector('main.calendar div.head h1.name');
+		const destination = document.querySelector('main.calendar section.head h1.name');
 	
 		// Update label for current month. 
 		destination.innerHTML = `${ monthfullname[currentMonthIndex] } ${currentYear}`;
@@ -88,7 +77,7 @@ function updateCalendar() {
 	function updateCalendarBody() {
 		
 		// Get destination. 
-		const destination = document.querySelector('div#container main.calendar div.body ul.monthdays');
+		const destination = document.querySelector('div#container main.calendar section.body ul.monthdays');
 	
 		// Initialize result. 
 		let result = '';
@@ -252,5 +241,153 @@ function deltaMonth(delta) {
 	else if(currentMonthIndex<0) {
 		currentMonthIndex += 12;
 		currentYear -= 1;
+	}
+}
+
+// Open month selector window. 
+function openSelector() {
+
+	// Get current year for newly opened selector. 
+	selectorCurrentYear = currentYear;
+
+	// Update selector. 
+	updateSelector();
+
+	// Activate selector. 
+	selector.classList.add('active');
+}
+
+// Update selector. 
+function updateSelector() {
+
+	// Fill body of selector. 
+	fillSelectorBody();
+
+	// Fill head of selector. 
+	fillSelectorHead();
+
+	// Activate selector. 
+	activateSelector();
+
+	/****/
+
+	// Add to selector: current year. 
+	function fillSelectorHead() {
+		selectorhead.innerHTML = selectorCurrentYear;
+	}
+
+	// Add to selector: month data for current year. 
+	function fillSelectorBody() {
+		
+		// Initialize result. 
+		let result = '';
+
+		// 
+		for(let i in monthData) {
+	
+			// 
+			let m = monthData[i];
+	
+			// 
+			result += `
+			<!-- month -->
+			<li class="month" data-yr="${selectorCurrentYear}" data-mo="${i}">
+	
+				<!-- caption -->
+				<span class="caption">${m.name}</span>
+				<!-- /caption -->
+	
+			</li>
+			<!-- /month -->`;
+		}
+
+		// Add result to selector body
+		selectorbody.innerHTML = result;
+	}
+
+	// Activate selector. 
+	function activateSelector() {
+		
+		// Get month buttons. 
+		const monthbtns = document.querySelectorAll('div#container main.calendar div.selector ul.select li.month');
+
+		// Go thru month buttons. 
+		for(let btn of monthbtns) {
+			btn.addEventListener('click',selectMonth)
+		}
+
+		/***/
+
+		// Select month. 
+		function selectMonth(event) {
+			// 
+			const btn = event.currentTarget;
+
+			// 
+			const yr = btn.getAttribute('data-yr');
+			currentYear = 1*yr;
+
+			// 
+			const mo = btn.getAttribute('data-mo');
+			currentMonthIndex = 1*mo;
+
+			// Update calendar. 
+			updateCalendar();
+
+			// Close month selector window. 
+			closeSelector();
+		}
+	}
+}
+
+// Shift current year of selector. 
+function shiftSelectorYr(delta) {
+
+	// Increment current selector year by delta. 
+	selectorCurrentYear += delta;
+
+	// Update selector. 
+	updateSelector();
+}
+
+// Close month selector window. 
+function closeSelector() {
+
+	// De-activate selector. 
+	selector.classList.remove('active');
+	
+	// Clear body of selector. 
+	selectorbody.innerHTML = '';
+	
+	// Clear head of selector. 
+	selectorhead.innerHTML = '';
+}
+
+// Activate keyboard shortcuts. 
+function activateShortcuts() {
+
+	// 
+	document.addEventListener('keyup',checkForShortcutKey);
+
+	/****/
+
+	// Check for shortcut key. 
+	function checkForShortcutKey(event) {
+		// console.log(event);
+
+		// Check if selector window open. 
+		let selectorOpen = selector.classList.contains('active');
+
+		// 
+		if(event.keyCode==83 || event.key=='s' || event.key=='S') openSelector();
+		else if(event.keyCode==27 || event.key=='Escape') closeSelector();
+		else if(event.keyCode==37 || event.key=='ArrowLeft') {
+			if(selectorOpen) shiftSelectorYr(-1);
+			else goToPrevMonth();
+		}
+		else if(event.keyCode==39 || event.key=='ArrowRight') {
+			if(selectorOpen) shiftSelectorYr(+1);
+			else goToNextMonth();
+		}
 	}
 }
