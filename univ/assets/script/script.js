2df8622db358628c63a96484afd686a9d2d2eba7
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-11-26 07:32:05 -0500

    Add files via upload

--- univ/assets/script/script.js
+++ univ/assets/script/script.js
@@ -627,8 +627,15 @@ function loadFooter() {
 
 	// Create site location breadcrumbs section. 
 	function createLocationTrail() {
-		// 
-		return `
+
+		// Initialize result. 
+		let result = ``;
+
+		// Append piece. 
+		result += ``;
+
+		// Append home piece. 
+		result += `
 		<!-- home -->
 		<a class="home" href="./">
 
@@ -640,17 +647,24 @@ function loadFooter() {
 			<!-- /logo -->
 
 		</a>
-		<!-- /home -->
+		<!-- /home -->`;
 
+		// Append arrow separator. 
+		result += `
 		<!-- icon -->
 		<svg class="icon rightarrow" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
 			<path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
 		</svg>
-		<!-- /icon -->
+		<!-- /icon -->`;
 
+		// Append page piece. 
+		result += `
 		<!-- node -->
 		<a class="node" href="./">PageName</a>
 		<!-- /node -->`;
+
+		// Return result. 
+		return result;
 	}
 }
 
