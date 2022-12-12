2c34cd7a4dfdbe0f60f985e7b990664aff901a41
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-12-12 02:51:21 -0500

    Add files via upload

--- hero/script/nav.js
+++ hero/script/nav.js
@@ -8,13 +8,19 @@ function toggleSideNav( openAlready = $('div#container').hasClass('navopen') ) {
 
 	// Close navigation if already open. 
 	if(openAlready) {
-		$('div#container').removeClass('navopen');
-		$('html,body').removeClass('stuck');
+		// $('div#container').removeClass('navopen');
+		document.querySelector('div#container').classList.remove('navopen');
+		// $('html,body').removeClass('stuck');
+		document.querySelector('html').classList.remove('stuck');
+		document.querySelector('body').classList.remove('stuck');
 	}
 	// Open navigation otherwise. 
 	else {
-		$('div#container').addClass('navopen');
-		$('html,body').addClass('stuck');
+		// $('div#container').addClass('navopen');
+		document.querySelector('div#container').classList.add('navopen');
+		// $('html,body').addClass('stuck');
+		document.querySelector('html').classList.add('stuck');
+		document.querySelector('body').classList.add('stuck');
 	}
 }
 
@@ -26,8 +32,13 @@ function highlightNavbarItem(event) {
 	let item = event.currentTarget;
 
 	// Un-highlight all other nav links. 
-	$('nav ul.navlist li.item a.link').removeClass('active');
+	// $('nav ul.navlist li.item a.link').removeClass('active');
+	xyz = document.querySelectorAll('nav ul.navlist li.item a.link')
+	for(x of xyz) {
+		x.classList.remove('active');
+	}
 
 	// Highlight clicked nav link. 
-	$(item).addClass('active');
+	// $(item).addClass('active');
+	item.classList.add('active');
 }
