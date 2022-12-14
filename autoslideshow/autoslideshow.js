5e64d78eae2ebe8c5c3d4e008107d736b1cc1c24
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-12-14 17:49:05 -0500

    Add files via upload

--- autoslideshow/autoslideshow.js
+++ autoslideshow/autoslideshow.js
@@ -3,15 +3,23 @@
 
 // Get page container. 
 const container = document.querySelector('div#container');
-// console.log(slideshow);
+// console.log('slideshow:',slideshow);
 
 // Get slideshow container. 
 const slideshow = document.querySelector('div#container main.slideshow');
-// console.log(slideshow);
+// console.log('slideshow:',slideshow);
 
 // Get slide container. 
 const slidecontainer = document.querySelector('div#container main.slideshow div.inner');
-// console.log(slidecontainer);
+// console.log('slidecontainer:',slidecontainer);
+
+// Get slideshow remote. 
+const slideshowremote = document.querySelector('div#container main.slideshow div.remote');
+// console.log('slideshowremote:',slideshowremote);
+
+// Get slide counter box. 
+const slideCounter = document.getElementById('slideCounter');
+// console.log('slideCounter:',slideCounter);
 
 
 /*****/
@@ -21,7 +29,7 @@ const slidecontainer = document.querySelector('div#container main.slideshow div.
 const numSlides = slideData.length;
 
 // Define slide interval (time per slide). 
-const slideInterval = 1000;
+const dt = 1500;
 
 // Define current slide index. 
 let currentSlideIndex = 0;
@@ -33,8 +41,8 @@ let slideMovement;
 /*****/
 
 
-// Load initial slide data. 
-loadSlideData();
+// Load slide show components. 
+loadSlideshow();
 
 // Start slide movement. 
 playSlideshow();
@@ -43,51 +51,60 @@ playSlideshow();
 /*****/
 
 
-// Load initial slide data. 
-function loadSlideData() {
-
-	// Initialize result. 
-	let result = '';
-
-	// Add slides. 
-	for(let i in slideData) {
-		// console.log(i);
-		result += createSlide(i);
-	}
-
-	// Add extra copy of first slide (for smooth transition from last back to first slide). 
-	result += createSlide(0);
+// Load slide show components. 
+function loadSlideshow() {
 
 	// Add resulting slides to page. 
-	slidecontainer.innerHTML = result;
+	slidecontainer.innerHTML = createSlides();
 
-	// Add remote controller. 
-	addRemote();
+	// Load remote controller. 
+	loadRemoteController();
 
 	/****/
 
-	// Create image slide. 
-	function createSlide(index) {
+	// Create slides. 
+	function createSlides() {
 
-		// Get slide item data. 
-		let slideItem = slideData[index]
+		// Initialize result. 
+		let result = '';
+	
+		// Add slides. 
+		for(let i in slideData) {
+			// console.log(i);
+			result += createSlideByIndex(i);
+		}
+	
+		// Add extra copy of first slide (for smooth transition from last slide back to first slide). 
+		result += createSlideByIndex(0);
+	
+		// Return result. 
+		return result;
 
-		// Return formatted slide item. 
-		return `
-		<!-- img -->
-		<img class="slide" src="${ slideItem.imageurl }" alt="${ slideItem.caption }" data-slideindex="${index}">
-		<!-- /img -->`;
+		/***/
+
+		// Create image slide by index. 
+		function createSlideByIndex(index) {
+	
+			// Get slide item data. 
+			let slideItem = slideData[index]
+	
+			// Return formatted slide item. 
+			return `
+			<!-- img -->
+			<img class="slide" src="${ slideItem.imageurl }" alt="${ slideItem.caption }" data-slideindex="${index}">
+			<!-- /img -->`;
+		}
 	}
 
-	// Add remote controller. 
-	function addRemote() {
+	// Load remote controller. 
+	function loadRemoteController() {
 
 		// Create remote controller. 
-		let remote = createRemote();
-		// console.log('remote:',remote);
+		let remotedots = createRemoteDots();
+		// console.log('remote dots:',remotedots);
 	
 		// Add remote controller to page. 
-		slideshow.insertAdjacentHTML('beforeend', remote );
+		slideshowremote.innerHTML = remotedots;
 
 		// Activate remote. 
 		activateRemote();
@@ -95,18 +112,16 @@ function loadSlideData() {
 		/***/
 
 		// Create remote controller. 
-		function createRemote() {
+		function createRemoteDots() {
 	
 			// Open remote. 
-			let result = `
-			<!-- remote -->
-			<div class="remote">`;
+			let result = '';
 			
 			// Add dot selector for each slide. 
 			for(let i=0 ; i<numSlides ; i++) {
 	
 				// Check for matching slide index. 
-				let onSelectedSlide = (i==currentSlideIndex);
+				let onSelectedSlide = i == (currentSlideIndex % numSlides);
 	
 				// Add dot. 
 				result += `
@@ -115,16 +130,11 @@ function loadSlideData() {
 				<!-- /dot -->`;
 			}
 	
-			// Close remote. 
-			result += `
-			</div>
-			<!-- /remote -->`;
-	
 			// Return result. 
 			return result;
 		}
 
-		// Activate remote. 
+		// Activate remote controller. 
 		function activateRemote() {
 
 			// Get all remote dots. 
@@ -143,74 +153,79 @@ function loadSlideData() {
 // Start slide movement. 
 function playSlideshow() {
 	
-	// Start automated slide changes and save interval id. 
-	slideMovement = setInterval(goToNextSlide,slideInterval);
+	// Start automated slide movement, saving id of movement. 
+	slideMovement = setInterval( goToNextSlide, dt );
+	console.log('Slide movement id:',slideMovement);
 
-	// Set state of slideshow. 
+	// Set state of slideshow: playing. 
 	slideshow.classList.add('active');
 
 	/****/
 
 	// Go to next slide. 
 	function goToNextSlide() {
-		console.log('goToNextSlide');
+		// console.log('goToNextSlide');
 
-		// Increment slide index. 
+		// Increment index for current slide. 
 		currentSlideIndex += 1;
 
 		// Show current slide. 
 		showSelectedSlide();
 
-		// Flip back to origin if on last slide. 
+		// Jump back to origin (if on last slide). 
 		if(currentSlideIndex==numSlides) {
 
 			// Reset slide position soon after showing last/first slide. 
-			setTimeout(resetSlidePosition, (1/4)*(slideInterval) );
+			setTimeout(jumpBack, .25*dt );
 		}
-	}
-
-	// Reset slide position back to origin. 
-	function resetSlidePosition() {
-
-		// Remove smooth transition before position reset. 
-		unsmoothIt();
-
-		// Reset slide index. 
-		currentSlideIndex = 0;
-		// Show current slide. 
-		setTimeout(showSelectedSlide, (1/4)*(slideInterval) );
-
-		// Add back smooth transition after position reset. 
-		setTimeout(smoothIt, (1/2)*(slideInterval) );
 
 		/***/
 
-		function smoothIt() {
-			slidecontainer.classList.add('smooth');
-		}
-
-		function unsmoothIt() {
-			slidecontainer.classList.remove('smooth');
+		// Reset slide position back to origin (first slide). 
+		function jumpBack() {
+	
+			// Remove transition for jump back to origin. 
+			unsmoothIt();
+	
+			// Reset slide index. 
+			currentSlideIndex = 0;
+			
+			// Show current slide. 
+			setTimeout(showSelectedSlide, .25*dt );
+	
+			// Add transition after jump back to origin. 
+			setTimeout(smoothIt, .5*dt );
+	
+			/**/
+	
+			function unsmoothIt() {
+				slidecontainer.classList.remove('smooth');
+			}
+	
+			function smoothIt() {
+				slidecontainer.classList.add('smooth');
+			}
 		}
 	}
 }
 // Pause slide movement. 
 function pauseSlideshow() {
 
-	// Clear automated call for slide movement. 
+	// Stop automated slide movement. 
 	clearInterval(slideMovement);
+	console.log('Slide movement id:',slideMovement);
 
-	// Set state of slideshow. 
+	// Set state of slideshow: not playing. 
 	slideshow.classList.remove('active');
 }
 // Toggle slideshow movement. 
 function toggleSlideshow() {
 
 	// Check if slideshow curently playing. 
-	let moving = slideshow.classList.contains('active');
+	let currentlyPlaying = slideshow.classList.contains('active');
 
 	// Pause if curently playing. 
-	if(moving) pauseSlideshow();
+	if(currentlyPlaying) pauseSlideshow();
 
 	// Play if not curently playing. 
 	else playSlideshow();
@@ -223,7 +238,7 @@ function selectNewSlide(event) {
 	let selectedDot = event.currentTarget;
 	console.log('Selected dot:',selectedDot);
 	
-	// Get and update selected slide index. 
+	// Update selected slide index. 
 	currentSlideIndex = 1 * selectedDot.getAttribute('data-slideindex');
 	console.log('Selected index:',currentSlideIndex);
 
@@ -233,28 +248,31 @@ function selectNewSlide(event) {
 // Show currently selected slide. 
 function showSelectedSlide() {
 
-	// Apply offset to slide container for currently selected slide. 
-	applySlideOffset();
+	// Update slide position. 
+	updateSlidePosition();
 
 	// Update state of remote controller. 
-	updateRemote(currentSlideIndex%numSlides);
+	updateRemoteController();
 
 	// Debug: Show slide index on page. 
-	showSlideIndex();
+	if(slideCounter) showSlideIndex();
 
 	/****/
 
-	// Apply offset to slide container for current slide index. 
-	function applySlideOffset() {
-		console.log('applySlideOffset');
+	// Update slide position (using current slide index). 
+	function updateSlidePosition() {
+		// console.log('updateSlidePosition');
+
+		// Calculate slide offset. 
+		let slideOffset = `${-100 * currentSlideIndex}%`;
 	
 		// Apply slide offset. 
-		slidecontainer.style.transform = `translateX(${ -100 * currentSlideIndex }%)`;
+		slidecontainer.style.transform = `translateX(${slideOffset})`;
 	}
 	
 	// Update state of remote controller. 
-	function updateRemote(slideIndex) {
-		console.log('updateRemote');
+	function updateRemoteController() {
+		// console.log('updateRemoteController');
 	
 		// Get all remote dots. 
 		const remoteDots = document.querySelectorAll('div#container main.slideshow div.remote span.dot');
@@ -263,11 +281,11 @@ function showSelectedSlide() {
 		// Apply remote offset. 
 		for(let dot of remoteDots) {
 	
-			// Get slide index of current dot. 
+			// Get slide index of current dot button. 
 			let i = 1 * dot.getAttribute('data-slideindex');
 	
 			// Check for matching slide index. 
-			let onSelectedSlide = (i==slideIndex);
+			let onSelectedSlide = i == (currentSlideIndex % numSlides);
 	
 			// Update state for active dot. 
 			if(onSelectedSlide) {
@@ -283,13 +301,9 @@ function showSelectedSlide() {
 
 	// Debug: Show current slide index on page. 
 	function showSlideIndex() {
-		console.log('showSlideIndex');
-
-		// Get slide counter box. 
-		const slideCounter = document.getElementById('slideCounter');
+		// console.log('showSlideIndex');
 		
 		// Show slide index in slide counter box. 
-		slideCounter.innerHTML = (currentSlideIndex % numSlides);
+		slideCounter.innerHTML = (currentSlideIndex/*  % numSlides */);
 	}
 }
-
