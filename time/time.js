14bceedaa83699dbe6629feba3c7c585769b39af
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-12-29 11:46:44 -0500

    Add files via upload

--- time/time.js
+++ time/time.js
@@ -1,24 +1,55 @@
 
 
 
+// Get input boxes. 
 const datebox = document.querySelector('div#container main.main div.input input.datebox');
 const timebox = document.querySelector('div#container main.main div.input input.timebox');
-const outputbox = document.querySelector('div#container main.main div.output');
-const resultbox = document.querySelector('div#container main.main div.output span.result');
+
+// Get output boxes. 
+const numoutputbox = document.querySelector('div#container main.main div.output.num');
+const numoutputresult = document.querySelector('div#container main.main div.output.num span.result');
+const sinceoutputbox = document.querySelector('div#container main.main div.output.since');
+const sinceoutputresult = document.querySelector('div#container main.main div.output.since span.result');
 
 
 /*****/
 
 
-// 
-// xyz();
+// Activate ability to copy result: time number. 
+activateResultCopy(numoutputbox);
+
+// Activate ability to copy result: time since string. 
+activateResultCopy(sinceoutputbox);
 
 
 /*****/
 
 
-// 
-function go() {
+// Activate ability to copy result. 
+function activateResultCopy(outputbox) {
+
+	// Copy result to clipboard upon click. 
+	outputbox.addEventListener('click',copyToClipboard);
+
+	// Reset tooltip to default upon mouse leave. 
+	outputbox.addEventListener('mouseleave',resetTooltip);
+
+	/****/
+
+	// Reset tooltip caption. 
+	function resetTooltip(event) {
+
+		// Get selected output box. 
+		let outputbox = event.currentTarget;
+		let tooltipbox = outputbox.querySelector('span.tooltip');
+
+		// Reset tooltip caption. 
+		tooltipbox.innerHTML = 'Copy to clipboard';
+	}
+}
+
+// Calculate number. 
+function calculateNumbers() {
 
 	// Get date input. 
 	let dateinput = datebox.valueAsNumber;
@@ -28,13 +59,11 @@ function go() {
 	let timeinput = timebox.valueAsNumber;
 	// console.log('timeinput:',timeinput,timebox);
 
-	// 
+	// Handle missing inputs. 
 	if(!dateinput) {
 		console.warn('Missing input: date');
 		return;
 	}
-
-	// 
 	if(!timeinput) {
 		console.warn('Missing input: time');
 		return;
@@ -53,18 +82,75 @@ function go() {
 	console.log('datetime:',datetime );
 
 	// 
-	resultbox.innerHTML = datetime.valueOf();
+	let n = datetime.valueOf();
 
-	// 
-	outputbox.style.backgroundColor = 'gold';
+	// Show number result. 
+	numoutputresult.innerHTML = n;
+
+	// Show time since result. 
+	sinceoutputresult.innerHTML = formatTimeSince(n);
+
+	/****/
+
+	// Get time since given number. 
+	// Format 'time since' number. 
+	function formatTimeSince(num) {
+
+		// Get number for current moment. 
+		let now = Date.now();
+
+		// Get estimated time since upload date (ETS). 
+		let dt = now - 1*num;
+		// console.log('dt:',dt);
+	
+		// Treat non-positive ets numbers as right now. 
+		if(dt<=0) return 'now';
+	
+		// Define components of formatted number. 
+		let coeff = dt;	// /1000/60/60/24/365
+		let suffindex = 0;
+		let suffix = [' millisecond',' second',' minute',' hour',' day',' week',' month',' year'];	// potential suffixes
+		let limits = [1000, 60, 60, 24, 7, 4.3, 12, 1000];
+	
+		// Divide number until... 
+		while(coeff>limits[suffindex]) {
+	
+			// Divide coefficient. 
+			coeff /= limits[suffindex];
+			// console.log('\tcoeff',coeff);
+	
+			// Increment suffix index. 
+			suffindex++;
+			// console.log('\tsuffindex',suffindex);
+		}
+	
+		// Add final formatting to view count. 
+		coeff = coeff.toFixed(0);
+		// let removeExtraZero = coeff.includes('.0');
+		// if(removeExtraZero) coeff = coeff.substr(0,coeff.length-2);
+	
+		return (coeff + suffix[suffindex]+((coeff>1)?('s ago'):(' ago')) );
+
+		// 
+		return num;
+	}
 }
 
-// Copy to clipboard. 
-function copyToClipboard() {
+// Copy selected result to clipboard. 
+function copyToClipboard(event) {
 
-	// Copy text from output onto clipboard. 
+	// Get output box. 
+	let outputbox = event.currentTarget;
+
+	// Get output result box. 
+	let resultbox = outputbox.querySelector('span.result');
+
+	// Get tooltip box. 
+	let tooltipbox = outputbox.querySelector('span.tooltip');
+
+	// Copy text from output result to clipboard. 
 	navigator.clipboard.writeText(resultbox.innerText);
 
 	// 
-	outputbox.style.backgroundColor = 'limegreen';
+	tooltipbox.innerHTML = `Copied: "${resultbox.innerText}"`;
 }
