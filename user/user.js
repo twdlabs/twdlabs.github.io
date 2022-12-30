23a60578d499768807d78c4f5a916c185ef4a32a
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2022-12-30 11:25:57 -0500

    Add files via upload

--- user/user.js
+++ user/user.js
@@ -1,46 +1,118 @@
 
 
-// Get navlist
-const navlist = document.querySelector('div#container main.user nav.menu ul.navlist');
 
+// Get user block. 
+const userblock = document.querySelector('div#container main.user');
 
-// Initialize result. 
-let result = '';
+// Get profile button. 
+const profilebtn = document.querySelector('div#container main.user a.profile');
 
-// Add list items to result. 
-for(let userListItem of userListData) {
-	result += createListItem(userListItem)
-}
+// Get navigation menu. 
+const navmenu = document.querySelector('div#container main.user nav.menu');
 
-// Add result to page. 
-navlist.innerHTML = result;
+// Get navigation list. 
+const navmenulist = document.querySelector('div#container main.user nav.menu ul.navlist');
 
 
 /*****/
 
 
-// Create nav list item. 
-function createListItem(item) {
-	return `
-	<!-- navitem -->
-	<li class="navitem">
+// Load navigation menu list. 
+loadMenuList();
 
-		<!-- navlink -->
-		<a class="navlink" href="${item.url}">
+// Activate profile button. 
+activateProfileBtn();
 
-			<!-- icon -->
-			<svg class="icon user" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-				${ iconData[item.icon] }
-			</svg>
-			<!-- /icon -->
 
-			<!-- caption -->
-			<span class="caption">${item.caption}</span>
-			<!-- /caption -->
+/*****/
+
+
+// Activate profile button. 
+function activateProfileBtn() {
+
+	// 
+	profilebtn.addEventListener('click',toggleNavMenu);
+
+	/****/
+
+	// Toggle navigation menu. 
+	function toggleNavMenu() {
+		// Toggle menu. 
+		userblock.classList.toggle('open');
+	}
+}
+
+// Load navigation menu list. 
+function loadMenuList() {
+
+	// Initialize result. 
+	let result = '';
+	
+	// Go thru all menu items. 
+	for(let menuItem of userListData) {
+
+		// Add menu item to result. 
+		result += createMenuItemLayout(menuItem)
+	}
+	
+	// Add result to page. 
+	navmenulist.innerHTML = result;
+
+	// Activate menu items. 
+	activateMenuItems();
+
+	/****/
+
+	// Create menu item. 
+	function createMenuItemLayout(item) {
+
+		// Compile components of menu item. 
+		return `
+		<!-- navitem -->
+		<li class="navitem">
+	
+			<!-- navlink -->
+			<a class="navlink" href="${item.url}">
+	
+				<!-- icon -->
+				<svg class="icon ${item.icon}" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+					${ iconData[item.icon] }
+				</svg>
+				<!-- /icon -->
+	
+				<!-- caption -->
+				<span class="caption">${item.caption}</span>
+				<!-- /caption -->
+				
+			</a>
+			<!-- /navlink -->
+	
+		</li>
+		<!-- /navitem -->`;
+	}
+
+	// Activate menu items. 
+	function activateMenuItems() {
+	
+		// Get all menu items. 
+		const allMenuItems = document.querySelectorAll('div#container main.user nav.menu ul.navlist li.navitem a.navlink');
+		
+		// Go thru all menu items. 
+		for(let menuItem of allMenuItems) {
 			
-		</a>
-		<!-- /navlink -->
+			// Enable menu item selection. 
+			menuItem.addEventListener('click',selectMenuItem);
+		}
+	
+		/***/
+	
+		// Select menu item. 
+		function selectMenuItem() {
+			
+			// Take action on menu item. 
 
-	</li>
-	<!-- /navitem -->`;
+			// Close menu. 
+			userblock.classList.remove('open');
+		}
+	}
 }
