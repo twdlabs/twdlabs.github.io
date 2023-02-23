bb51ad356fc8ed2cf80cbbb8adf7cde763317392
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-02-23 12:13:02 -0500

    Add files via upload

--- user/database/userdatabase.js
+++ user/database/userdatabase.js
@@ -1,303 +1,526 @@
 
 
 
-// Define list of user data. 
-const userDataList = [
+// Define default list of user data. 
+const defaultUserDataList = [
 
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Ace',
 		lname:'Ventura',
+		fullname:'',
 		admin:true,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Bill',
 		lname:'Diamond',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Christine',
 		lname:'Brophy',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'David',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Eli',
 		lname:'Jah',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Fred',
 		lname:'Flinstone',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'George',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Hilary',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Ian',
 		lname:'Robertson',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Jane',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Jay',
 		lname:'Jenkins',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Jenny',
 		lname:'Joe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Jill',
 		lname:'Jackson',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Jim',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Joe',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'John Luke',
 		lname:'Smith',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Kate',
 		lname:'Croix',
+		fullname:'',
 		admin:true,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Leah',
-		lname:'Lemons',
+		lname:'Lloyd',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Mike',
 		lname:'Myers',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Nina',
 		lname:'Nash',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Oscar',
 		lname:'DeLaHoya',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Peter',
 		lname:'Pan',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Quinten',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Rachel',
 		lname:'Roy',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Steve',
 		lname:'Stephens',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Tim',
-		lname:'Tool',
+		lname:'Toole',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Tori',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Ursula',
 		lname:'Doe',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Victor',
-		lname:'Victory',
+		lname:'Victorian',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Wendy',
 		lname:'Will',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Xavier',
 		lname:'Toven',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-m.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 	{
-		// userid:'xyzabcxyz',
+		userid:'',
 		fname:'Yolanda',
 		lname:'Yokes',
+		fullname:'',
 		admin:false,
+		email:'',
+		password:'',
+		mobilenumber:'',
 		avatarurl:'./images/avatar-f.png',
-		position:'Website Designer',
+		position:'',
+		bio:'',
 	},
 
 ];
 
-
-/*****/
-
-
-// Add supplemental user data. 
-addUserData();
+// Initialize list of user data. 
+let userDataList = [];
 
 
 /*****/
 
 
-// Add supplemental user data. 
-function addUserData() {
+// Add supplemental user data to default list. 
+completeDefaultUserData();
 
-	// 
-	for(let user of userDataList) {
+
+/*****/
+
+
+// Add supplemental user data to default list. 
+function completeDefaultUserData() {
+
+	// Go thru all user data in default list. 
+	for(let user of defaultUserDataList) {
 
 		// Auto-generate user id from name. <--could be a function
 		user.userid = `${ (user.fname).substring(0,1) }${ user.lname }`.toLowerCase().replace(' ','');
-
 		// Auto-generate full name. <--could be a function
 		user.fullname = `${user.fname} ${user.lname}`;
 
 		// Auto-generate default email address. <--could be a function
 		user.email = `${ user.userid }@mail.com`;
-
-		// Add default mobile number. 
-		user.mobilenumber = '+1 (213) 456-7890';
-
 		// Add default password. 
 		user.password = user.userid+'!';
+		// Add default mobile number. 
+		user.mobilenumber = createPhoneNumber();
 
 		// Auto-generate default bio. 
-		user.bio = `This is a brief bio for ${ user.fname } ${ user.lname }, whose username is ${ user.userid }. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint consequatur tempore! Doloremque est dolorum modi repellat aspernatur non consequatur quia, dignissimos optio nemo omnis quidem provident ipsam commodi animi.`;
+		user.position = 'Website Designer';
+		user.bio = `This is a brief bio of ${ user.fullname }, a ${ user.position } who has the username \'${ user.userid }\'. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint consequatur tempore! Doloremque est dolorum modi repellat aspernatur non consequatur quia, dignissimos optio nemo omnis quidem provident ipsam commodi animi.`;
+	}
+
+	/****/
+
+	// Create phone number. 
+	function createPhoneNumber() {
+
+		// 
+		let result = '';
+		result += `+1 (${ getAreaCode() }) `;
+		// result += '456';
+		// result += '-';
+		// result += '7890';
+		result += getRandomDigit(1,9);
+		result += getRandomDigit(0,9);
+		result += getRandomDigit(0,9);
+		result += '-';
+		result += getRandomDigit(0,9);
+		result += getRandomDigit(0,9);
+		result += getRandomDigit(0,9);
+		result += getRandomDigit(0,9);
+
+		// 
+		return result;
+
+		/***/
+
+		// Get area code. 
+		function getAreaCode() {
+	
+			// Define list of area codes (courtesy of Ludacris & Nate Dogg). 
+			const areaCodeList = [
+				770,404,718,202,901,
+				305,312,313,215,803,
+				757,410,213,212,504,
+				972,713,314,201,212,
+				213,916,415,704,206,
+				808,216,702,414,317,
+				214,281,334,205,318,
+				601,203,804,402,301,
+				904,407,850,
+			];
+	
+			// Get number of values. 
+			let n = areaCodeList.length;
+	
+			// Choose a random index. 
+			let i = Math.floor( n * Math.random() );
+	
+			// Return random area code. 
+			return areaCodeList[i];
+		}
+
+		// Get random digit between given numbers. 
+		function getRandomDigit(a,b) {
+
+			// 
+			let n = (b - a);
+
+			// 
+			return Math.floor( a + (n * Math.random()) );
+		}
 	}
 }
 
