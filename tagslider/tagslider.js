dbd0c3d7f04ffc948388e79ac82c85976312c46d
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-06-19 12:57:39 -0400

    Add files via upload

--- tagslider/tagslider.js
+++ tagslider/tagslider.js
@@ -1,83 +1,158 @@
 
 
 
-// Initialize dragging state of tag list bin. 
-let currentlyDragging = false;
-
 // Get tag list slider. 
 const slider = document.querySelector('div#container main.main div.slider');
 
 // Get tag list slider bin. 
-const taglist = document.querySelector('div#container main.main div.slider ul.taglist');
+const tagbox = document.querySelector('div#container main.main div.slider ul.taglist');
 
-// Get root element. 
-const root = document.querySelector(':root');
+// Get tag list slider bin. 
+const tagitems = document.querySelectorAll('div#container main.main div.slider ul.taglist li.tagitem');
+
+// Get slider edges. 
+const leftedge = document.querySelector('div#container main.main div.edge.left');
+const rightedge = document.querySelector('div#container main.main div.edge.right');
+
+// Get slide buttons. 
+// const slidebtns = document.querySelectorAll('div#container main.main div.edge div.slidebtn');
+// const leftslidebtn = document.querySelector('div#container main.main div.edge.left div.slidebtn');
+// const rightslidebtn = document.querySelector('div#container main.main div.edge.right div.slidebtn');
+
+// Initialize dragging state of tag list bin. 
+let isCurrentlyDragging = false;
+
+// Define edge width (in rem). 
+let edgewidth = 4*16;
 
 
 /*****/
 
 
-// 
-taglist.addEventListener('mousedown', ()=> currentlyDragging = true );
-taglist.addEventListener('mousemove', draggingMotion );
-document.addEventListener('mouseup', ()=> currentlyDragging = false );
+// Activate tags. 
+activateTags();
+
+// Activate tag slider. 
+activateTagSlider();
+
+// Update button visibility. 
+updateBtnVisibility();
 
 
 /*****/
 
 
-// 
-function draggingMotion(event) {
-	if(!currentlyDragging) return;
+// Activate tags. 
+function activateTags() {
 
-	// Get horizontal offset. 
-	let dx = event.movementX;
+	// Activate tags. 
+	for(let tag of tagitems) {
+		// Select tag. 
+		tag.addEventListener('click',selectTag);
+	}
 
-	// 
-	taglist.scrollLeft -= dx
+	/****/
+	
+	// Select tag. 
+	function selectTag(event) {
+
+		// Un-highlight previously selected tag. 
+		tagbox.querySelector('.active').classList.remove('active');
+
+		// Highlight newly selected tag. 
+		tag = event.currentTarget;
+		tag.classList.add('active');
+	}
 }
 
-// Slide tags in leftward direction. 
+// Activate tag slider. 
+function activateTagSlider() {
+
+	// Start dragging. 
+	tagbox.addEventListener('mousedown', startDragging );
+
+	// Ccontinue dragging. 
+	tagbox.addEventListener('mousemove', continueDragging );
+
+	// Finish dragging. 
+	document.addEventListener('mouseup', finishDragging );
+
+	/****/
+
+	// Start dragging. 
+	function startDragging(event) {
+
+		// Turn on drag mode. 
+		isCurrentlyDragging = true;
+	}
+
+	// Ccontinue dragging. 
+	function continueDragging(event) {
+
+		// Ignore mouse movement when not in drag mode. 
+		if(!isCurrentlyDragging) return;
+
+		// Allow insta-movement of slider. Prevent tag clicks. 
+		slider.classList.add('dragmode');
+	
+		// Get horizontal movement. 
+		let dx = event.movementX;
+		console.log('dx:',dx);
+	
+		// Apply horizontal offset. 
+		tagbox.scrollLeft += dx;
+		console.log('scrollLeft:',tagbox.scrollLeft);
+	}
+
+	// Finish dragging. 
+	function finishDragging(event) {
+
+		// Turn off drag mode. 
+		isCurrentlyDragging = false;
+		slider.classList.remove('dragmode');
+
+		// Update button visibility. 
+		updateBtnVisibility();
+	}
+}
+
+// Update button visibility. 
+function updateBtnVisibility() {
+
+	// Check current scroll status. 
+	let allWayLeft = (slider.scrollLeft == 0);
+	// console.log('allWayLeft:',allWayLeft);
+	let allWayRight = (slider.scrollLeft == slider.scrollLeftMax);
+	// console.log('allWayRight:',allWayRight);
+
+	// Hide left button if all the way left. 
+	if(allWayLeft) leftedge.classList.add('gone');
+	// Show left button otherwise. 
+	else leftedge.classList.remove('gone');
+
+	// Hide right button if all the way right. 
+	if(allWayRight) rightedge.classList.add('gone');
+	// Show right button otherwise. 
+	else rightedge.classList.remove('gone');
+}
+
+
+// Show more tags from leftward direction. 
 function slideLeft() {
 
-	// Get previous slide percentage. 
-	let pct = root.style.getProperyValue('--currentslidepercent');
+	// Decrement scroll position. 
+	slider.scrollLeft += -(slider.clientWidth-2*edgewidth);
 
-	// Decrement slide percentage to new value. 
-	pct -= 10;
-
-	// Update to new slide percentage. 
-	updateSlidePercent(pct);
+	// Update button visibility. 
+	updateBtnVisibility();
 }
 
-// Slide tags in rightward direction. 
+// Show more tags from rightward direction. 
 function slideRight() {
 
-	// Get previous slide percentage. 
-	let pct = root.style.getProperyValue('--currentslidepercent');
+	// Increment scroll position. 
+	slider.scrollLeft += (slider.clientWidth-2*edgewidth);
 
-	// Increment slide percentage to new value. 
-	pct += 10;
-
-	// Update to new slide percentage. 
-	updateSlidePercent(pct);
+	// Update button visibility. 
+	updateBtnVisibility();
 }
-
-// Update slide percentage (scroll position). 
-function updateSlidePercent(pct) {
-
-	// Get max scroll position. 
-	let maxscrollposition = slider.scrollLeftMax;
-
-	// Limit slide percentage to values within range. 
-	if(pct<0) pct = 0;
-	else if(pct>100) pct = 100;
-
-	// Set new slide percentage. 
-	root.style.setPropery('--currentslidepercent',pct);
-
-	// Apply slide percentage. 
-	let pos = (pct/100) * maxscrollposition;
-	taglist.style.transform = `translateX(${pos}px)`;
-}
-
