001ee219c8f52c5c1f9bc49f54d1e0937cfe7252
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-07-09 01:52:16 -0400

    Add files via upload

---
+++ share/share.js
@@ -0,0 +1,13 @@
+
+
+
+// Get container. 
+const container = document.querySelector('div#container');
+const linkfield = document.querySelector('div#container main.main div.linkfield input.link');
+
+
+/*****/
+
+
+// Set link. 
+linkfield.value = window.location;
