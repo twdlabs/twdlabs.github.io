60465fbec85ed2884616ab1ae73d41c90fbb31d9
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-07-12 08:48:38 -0400

    Add files via upload

--- navcircle/navcircle.js
+++ navcircle/navcircle.js
@@ -1,11 +1,14 @@
 
 
 
+// Get navigation circle. 
+const navcircle = document.querySelector('div#container nav.navcircle');
+
 // Get toggle button. 
 const togglebtn = document.querySelector('div#container nav.navcircle div.togglebtn');
 
 // Get navigation circle list. 
-const navcirclelist = document.querySelector('div#container nav.navcircle ul.navlist');
+// const navcirclelist = document.querySelector('div#container nav.navcircle ul.navlist');
 
 
 /*****/
@@ -14,11 +17,14 @@ const navcirclelist = document.querySelector('div#container nav.navcircle ul.nav
 // Activate toggle button. 
 togglebtn.addEventListener('click',toggleNavCircle);
 
+// Activate mobility of navigation circle. 
+makeItemMovable(navcircle,'div.togglebtn');
+
 
 /*****/
 
 
 // Toggle navigation circle. 
 function toggleNavCircle() {
-	navcirclelist.classList.toggle('on');
+	navcircle.classList.toggle('on');
 }
