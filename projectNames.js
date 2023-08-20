595ca2bb8ba1e5f04d4fd7ba73e13a6498b26e93
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-08-20 17:27:24 -0400

    Add files via upload

--- projectNames.js
+++ projectNames.js
@@ -2,35 +2,51 @@
 
 // List project names.
 const projectNames = [
-	'3d','10000cards','360finance',
-	'accordion','admin','admin2','affirm','agency','analogclock','animation','apple','application','aspectratio','atg','audio','autocomplete','autoslideshow',
-	'backToTop','band','battlerap','bell','big3dcard','blog','bodyshop','book','bookmark','books','box','boxshadow','breadcrumbs','bs5','bubblesort','buttonpress',
-	'calc','calculator','calendar','cali','caliB','canvas','cardflipgallery3d','cardslider','carnell','carnell2','carousel','carousel3d',
-	'chadpiff','charlimit','chart','chat','checkout','checkout2','chooser','chooser-old','cis255','clearfix','clipboard','clock','clone','clubhouse','cluster','cluster1d','cms',
-	'code','cognitivebias','collapsible','color','comingsoon','contact','contactform','contextmenu', 'cookie','copyright','countdown','covid','crm','crud','crud2','cubecarousel','cutout','cv',
-	'dashboard','db','decision','deck','delta','depth','device','diagram','dice','dollar','dotcollector','draggable','dragndrop',
-	'eazechain','editor','enter','essay','event','evolution','ezdev',
-	'fam','family','filter','finance','fitness','flashlight','flex','flexbreak','font','form','form3d','format','formvalidation',
-	'gallery','gallerywalk','gametapes','gitcalendar','goodz','gooey','gradient','graph','grid','grocery','grocery2','guess',
+	'10000cards','100pure','360finance','3d','3dnav',
+	'accordion','accordion2','admin','admin2','affirm','agency','analogclock','angeldemon','animation','anime','apple','application','aspectratio','atg','atm','audio','autocomplete','autoslideshow',
+	'backToTop','band','banner','battlerap','bell','big3dcard','bizmath','blockmenu','blog','bloglist','blogsocial','blogtoggler','bodyshop','book','bookmark','books','bounce','box','boxshadow','breadcrumbs','bs5','bst','bubblesort','buttonpress','buy',
+	'calc','calculator','calendar','calendar2','cali','caliB','canvas','cardflipgallery3d','cardstacks','carnell','carnell2','carousel','carousel3d',
+	'chadpiff','charlimit','chart','chat','checkout','checkout2','chess','chooser','chooser-old','cis255','clearfix','clipboard','clock','clockspinner','clone','clubhouse','cluster','cluster1d','cms',
+	'code','cognitivebias','collapsible','colorpicker','colorpicker2','comingsoon','compound','contact','contactform','contextmenu', 'cookie','copyright','countdown','course','covid','crm','crud','crud2','cubecarousel','cutout','cv',
+	'dashboard','db','decision','deck','delta','depth','desktop','device','diagram','dice','dollar','dotcollector','dotnav','dragndrop','dragndrop1','dragndroplist','dynamicnavbar',
+	'eazechain','editor','education','enter','essay','event','evolution','ezdev',
+	'fam','family','filter','finance','fitness','flashlight','flex','flexbreak','floatmenu','font','form','form3d','format','formvalidation','funnel',
+	'gallery','gallerywalk','gameboard','gametapes','gitcalendar','glow','goodz','gooey','gradient','graph','grid','grocery','grocery2','guess',
 	'hamburger','hero','hero2','hexagon','hovercard',
 	'icon','iglive','igprofile','igt','image','imagefilter','input','instagram','inventory',
 	'jay','jenna','json',
-	'keyframes','keygenius','keypress','koan','kumba',
-	'land','layouts','leads','learner','levels','lightSwitch','linktree','livesearch','loader','loading','logo',
-	'macbook','mandorespect','maskimage','masonry','math','matrix','medialibrary','mementomori','mentalmodels','merch','message','minion','music','musicplayer','musicplaylist',
-	'navbar','netflix','neuro67','news','notepad','notes',
-	'overflow','overlay',
-	'pageNexter','personality','perspective','pgi','phonecall','pinyin','plax','plax2','podcast','pokeball','pong','popup','portfolio','postercircle','pricing','probability','profilecard','progressbar','prototype',
-	'rating','rating2','readmore','reast','refresh','regexp','resume','retool','robinhood',
-	'scale3d','scroller','scrollProgressBar','scrollsnap','searchbar','select','shape','sidenav','signin','skills','sky','slideshow','slideshow2','smoothscroll','smosa',
-	'snapslider','socialmedia','solarsystem','sort','soundfx','spades','sphere','spinner','splash','squarespace','stepprogressbar','stocks','storage','storybrand','student','superforg','svsu','switcher','syntax',
-	'tabs','tabs3d','tagboard','tags','tasks','template','testimonial','theme','theme2','theme3','thermometer','ticker','timeline','timeline2','toast','toastprogress','todo','topnav','traffic','travisscott','trifold3d','trig','tropicalghana','typer','typetest','typewriter',
-	'univ','unseenstrts','urlparams','user','user2',
-	'valueslider','vanilla','video','video2','viewport','vidbg','vidbg2','voisey',
-	'watchthescreen','waves','wordcube','wts',
+	'keyfinder','keyframes','keygenius','keypress','koan','kumba',
+	'lab','land','lander','laundry','layouts','leads','learner','levels','lightSwitch','linktree','livesearch','loader','loading','login','login2','logo',
+	'macbook','mandorespect','maskimage','masonry','math','matrix','medialibrary','megamenu','mementomori','mentalmodels','multimenu','merch','message','minion','multimenu','music','musicplayer','musicplaylist',
+	'navbar','navcircle','netflix','neuro67','news','notepad','notes','notesquares',
+	'online','orbit','overflow','overlay',
+	'pageNexter','pageslider','pagination','payment','periodictable','personality','perspective','pgi','phonecall','pixelart','pinyin','plax','plax2','podcast','pokeball','pong','popup','portfolio','portfolio1','postercircle','pricing','prime','probability','profilecard','progressbar','prototype',
+	'rating','rating2','reast','refresh','regexp','relativelink','resume','resume2','retool','robinhood','rolodex',
+	'safari','scale3d','scroller','scrollProgressBar','scrollsnap','searchbar','select','sermons','shape','share','sidenav','signin','skeleton','skills','sky','sliderboard','slideshow','slideshow2','smoothscroll','smosa',
+	'snapslider','socialbtns','socialmedia','solarsystem','sort','soundfx','spades','sphere','spinner','splash','squarespace','stepprogress','stepprogressbar','stocks','storage','storybrand','student','superforg','svsu','switcher','syntax',
+	'tabs','tabs3d','tagboard','tags','tagslider','tasks','taxes','team','template','testimonial','theme','theme1','theme2','thermometer','throttle','ticker','time','timeline','timeline2','timing','toast','toastprogress','todo','topnav','touch','traffic','travisscott','trifold3d','trig','tropicalghana','typer','typetest','typewriter',
+	'univ','unseenstrts','upload','upload1','urlparams','urlparams2','user','user2',
+	'valueslider','vanilla','video','video2','viewport','vidbg','vidbg2','voisey','vsl',
+	'wave','waves','wavyloader','wordcube','wordswitcher','wts','wts0','wts1','wts2',
+	'xhr',
 	'ytblp',
 	'zbar'
 ];
+
+
+
+// Create xyz. 
+function createXyz() {
+
+	// Initialize result. 
+	let result = '';
+
+	// Add to result. 
+	result += ``;
+
+	// Return result. 
+	return result;
+}
 
 
 
