39d9871f00992e7999b0630cd4b20dbabdd4880a
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-09-26 17:58:51 -0400

    Add files via upload

--- clipboard/assets/clipboard.js
+++ clipboard/assets/clipboard.js
@@ -1,46 +1,84 @@
 
 
 
-// Get text field. 
-const textbox = document.querySelector('input.text');
-console.log('Text box:',textbox);
+// Get input text field. 
+const textfield = document.querySelector('div#container main.main div.item input.text');
+console.log('Text field:',textfield);
 
 // Get copy button. 
 const copybtn = document.querySelector('div#container main.main div.item button.copybtn');
 console.log('Copy button:',copybtn);
 
 // Get tooltip on copy button. 
-const tooltip = document.querySelector('div#container main.main div.item button.copybtn span.tooltip');
-console.log('Button tooltip:',tooltip);
+const copybtntooltip = document.querySelector('div#container main.main div.item button.copybtn span.tooltip');
+console.log('Copy button tooltip:',copybtntooltip);
+
+// Define time delay. 
+const dt = 2500;
+
+// Initialize timed reset. 
+let timedreset;
 
 
 /*****/
 
 
-// Reset tooltip contents when mouse leaves. 
-copybtn.addEventListener('mouseout',resetTooltip);
+// Copy input text to clipboard upon button click. 
+copybtn.addEventListener('click',copyToClipboard);
+
+// Reset tooltip contents upon mouse leaving button. 
+copybtn.addEventListener('mouseleave',resetCopyBtn);
 
 
 /*****/
 
 
-// Copy text to clipboard. 
+// Copy input text to clipboard. 
 function copyToClipboard() {
 
+	// Copy text from text field onto clipboard. 
+	navigator.clipboard.writeText(textfield.value);
+
 	// Select text in text field. 
-	textbox.select();
-	textbox.setSelectionRange(0,99999);
+	selectTextField();
 
-	// Copy text from text field. 
-	navigator.clipboard.writeText(textbox.value);
+	// Update state of copy button. 
+	updateCopyBtn();
+	// Reset state of copy button after time delay. 
+	timedreset = setTimeout(resetCopyBtn,dt);
 
-	// Update contents of tooltip. 
-	tooltip.innerHTML = `Copied: ${textbox.value}`;
+	/****/
+
+	// Select text in text field. 
+	function selectTextField() {
+		// Select text on computer. 
+		textfield.select();
+		// Select text on mobile. 
+		textfield.setSelectionRange(0,99999);
+	}
 }
 
-// Reset tooltip. 
-function resetTooltip() {
+// Update state of copy button. 
+function updateCopyBtn() {
+
+	// Update tooltip caption. 
+	copybtntooltip.innerHTML = 'Copied';
+	copybtntooltip.innerHTML = `Copied: "${textfield.value}"`;
+
+	// Update button state. 
+	copybtn.classList.add('done');
+}
+
+// Reset state of copy button. 
+function resetCopyBtn() {
+
+	// Reset tooltip caption. 
+	// copybtntooltip.innerHTML = 'Copy text';
+	copybtntooltip.innerHTML = 'Copy to clipboard';
+
+	// Reset button state. 
+	copybtn.classList.remove('done');
 	
-	// Reset contents of tooltip. 
-	tooltip.innerHTML = 'Copy to clipboard';
+	// Clear timed reset. 
+	clearTimeout(timedreset);
 }
