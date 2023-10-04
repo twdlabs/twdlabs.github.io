8c79f7dfecf853a98ae242e7fb412faf26f1e4d5
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-10-04 10:49:55 -0400

    Add files via upload

---
+++ quiz/assertivenessdatabase.js
@@ -0,0 +1,46 @@
+
+
+
+// Define assertiveness questionnaire data. 
+const assertivenessdata = [
+	{
+		question:`You feel you deserve the corner office that has Judy become available. What would you do?`,
+		answers:[
+			`Stay quiet and hope the boss realizes that you deserve the office. `,
+			`Hint around that you have outgrown your cubicle. `,
+			`Tell your coworkers, “You deserve it more than I do,” but secretly ask the boss if you can have it. `,
+			`Meet with your supervisor and lay out the reasons you think you deserve the office`,
+			`Threaten to quit if you aren’t assigned to the office`,
+		],
+	},
+	{
+		question:`Your best friend just called to cancel your weekend trip together at the last minute. This isn’t the first time your friend has done this, and you are very disappointed. What do you do?`,
+		answers:[
+			`Reassure your friend that it’s okay and there are no hard feelings. `,
+			`Declare, “But I’ve already packed,” hoping your friend will take the hint and decide to go after all. `,
+			`Resolve to cancel the next trip yourself to teach your friend a lesson. `,
+			`Say, “I feel disappointed, because I enjoy my time with you and because we have made nonrefundable deposits. Can we work this out?”`,
+			`Announce that the friendship is over. That’s no way to treat someone you care about. `,
+		],
+	},
+	{
+		question:`During a classroom discussion, a fellow student makes a comment that you find offensive. What do you do?`,
+		answers:[
+			`Ignore it. `,
+			`Tell the instructor after class that the comment made you uncomfortable. `,
+			`Say nothing, but tell other people how much you dislike that person. `,
+			`Join the discussion, say that you see the issue differently, and invite your classmate to explain why they feel as they do. `,
+			`Announce that the statement is the most stupidest thing you have ever heard. `,
+		],
+	},
+	{
+		question:`You are on a first date when the other person suggests seeing a movie you are sure you will hate. What do you do?`,
+		answers:[
+			`Say, “Sure!” How bad can it be?`,
+			`Lie and say you’ve already seen it. `,
+			`Say, “Okaaay,” and raise your eyebrows in a way that suggests your date must be either stupid or kidding. `,
+			`Suggest that you engage in another activity instead. `,
+			`Proclaim that you’d rather stay home and watch old reruns than see that movie. `,
+		],
+	},
+];
