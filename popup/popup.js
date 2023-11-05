b09c9e8536acea143d4741da9a86ad7370370969
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-11-05 09:17:05 -0500

    Add files via upload

--- popup/popup.js
+++ popup/popup.js
@@ -1,29 +1,36 @@
 
 
-// window.onload = openPopup;
+
+// Get overlay popup. 
+const overlaypopup = document.getElementById('overlay');
+
+
+/*****/
+
+
+// Open popup in due time. 
+setTimeout(openPopup,15000);
+
+
+/*****/
+
 
 // Open popup. 
 function openPopup(){
 
-	// Show popup element. 
-	document.getElementById('overlay').classList.add('active');
-	// $('#overlay').fadeIn(500).addClass('active');
+	// Show overlay popup. 
+	overlaypopup.classList.add('active');
 
 	// Freeze body scrolling. 
 	document.body.classList.add('freeze');
-	// $('body').addClass('freeze');
-	// $('body').css('overflow','hidden');
 }
 
 // Close popup. 
 function closePopup(){
 
-	// Hide popup element. 
-	document.getElementById('overlay').classList.remove('active');
-	// $('#overlay').fadeOut(500).removeClass('active');
+	// Hide overlay popup. 
+	overlaypopup.classList.remove('active');
 
 	// Un-freeze body scrolling. 
 	document.body.classList.remove('freeze');
-	// $('body').removeClass('freeze');
-	// $('body').css('overflow','');
 }
