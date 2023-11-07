8a86e34a00145ff6e7437ee003155867f58a6fb8
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-11-07 17:08:52 -0500

    Add files via upload

--- minion/minion.js
+++ minion/minion.js
@@ -24,27 +24,47 @@ for(let e of events) {
 
 // Check for positioning of pointer. 
 function checkPositioning(event) {
+
+	// Get position of pointer. 
+	let ppos = getPointerPosition();
 	
 	// Go for each eyeball. 
 	for(let eye of eyeballs) {
 		// console.log('eye:',eye);
 
-		// Get position of eye. 
-		let epos = getEyePosition(eye);
-
-		// Get position of pointer. 
-		let ppos = getPointerPosition();
-
-		// Get positional differential between pointer and eyeball. 
-		let dx = ppos.x - epos.x;
-		let dy = ppos.y - epos.y;
-		// console.log('(dx,dy):', dx,dy);
-
 		// Set rotation angle for eyeball. 
-		setRotationAngle(eye, dx,dy);
+		setRotationAngle(eye);
 	}
 
 	/****/
+
+	// Get position of pointer. 
+	function getPointerPosition() {
+
+		// Check for touch screen device. 
+		let useTouch = isTouchDevice();
+
+		// Get position of user's pointer (from top-left corner). 
+		let x = useTouch ? (event.touches[0].clientX) : (event.clientX);
+		let y = useTouch ? (event.touches[0].clientY) : (event.clientY);
+		// console.log('pointer (x,y):', x,y);
+
+		// Return position. 
+		return {x:x,y:y};
+
+		/***/
+
+		// Check for touch screen device. 
+		function isTouchDevice() {
+			try {
+				document.createEvent('TouchEvent');
+				return true;
+			}
+			catch(e) {
+				return false;
+			}
+		}
+	}
 
 	// Get position of eye. 
 	function getEyePosition(eye) {
@@ -68,36 +88,16 @@ function checkPositioning(event) {
 		return {x:x,y:y};
 	}
 
-	// Get position of pointer. 
-	function getPointerPosition() {
-
-		// Check for touch screen device. 
-		let useTouch = isTouchDevice();
-
-		// Get position of user's pointer (from top-left corner). 
-		let x = useTouch ? (event.touches[0].clientX) : (event.clientX);
-		let y = useTouch ? (event.touches[0].clientY) : (event.clientY);
-		console.log('pointer (x,y):', x,y);
-
-		// Return position. 
-		return {x:x,y:y};
-
-		/***/
-
-		// Check for touch screen device. 
-		function isTouchDevice() {
-			try {
-				document.createEvent('TouchEvent');
-				return true;
-			}
-			catch(e) {
-				return false;
-			}
-		}
-	}
-
 	// Set rotation angle for eyeball. 
-	function setRotationAngle(eye, dx,dy) {
+	function setRotationAngle(eye) {
+
+		// Get position of eye. 
+		let epos = getEyePosition(eye);
+
+		// Get positional differential between pointer and eyeball. 
+		let dx = ppos.x - epos.x;
+		let dy = ppos.y - epos.y;
+		// console.log('(dx,dy):', dx,dy);
 
 		// Get angle of pointer position (from radians to degrees). 
 		let radAngle = Math.atan2(dx, dy);
