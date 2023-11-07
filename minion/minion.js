90410816807c760727e509d3b21c1882ec76151e
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-11-07 17:03:23 -0500

    Add files via upload

--- minion/minion.js
+++ minion/minion.js
@@ -1,73 +1,113 @@
 
 
+
 // Get eyeball elements. 
 let eyeballs = document.querySelectorAll('.eye');
 // console.log('eyeballs:',eyeballs);
 
-// List events to watch for. 
+// List event types to watch for. 
 let events = ['mousemove','touchmove'];
 // console.log('events:',events);
+
+
+/*****/
+
 
 // Go for each event type. 
 for(let e of events) {
 	document.body.addEventListener(e, checkPositioning);
 }
 
+
 /*****/
 
-// Check for positioning. 
+
+// Check for positioning of pointer. 
 function checkPositioning(event) {
+	
 	// Go for each eyeball. 
 	for(let eye of eyeballs) {
 		// console.log('eye:',eye);
 
-		// Get bounding rectangle of eyeball (DOM). 
-		let rect = eye.getBoundingClientRect();
+		// Get position of eye. 
+		let epos = getEyePosition(eye);
+
+		// Get position of pointer. 
+		let ppos = getPointerPosition();
+
+		// Get positional differential between pointer and eyeball. 
+		let dx = ppos.x - epos.x;
+		let dy = ppos.y - epos.y;
+		// console.log('(dx,dy):', dx,dy);
+
+		// Set rotation angle for eyeball. 
+		setRotationAngle(eye, dx,dy);
+	}
+
+	/****/
+
+	// Get position of eye. 
+	function getEyePosition(eye) {
+
+		// Get size of eyeball. 
 		let w = eye.clientWidth;
 		let h = eye.clientHeight;
-		// console.log('rectangle:',rect);
 		// console.log('w:',w);
 		// console.log('h:',h);
 
+		// Get position of eyeball (relative to page?). 
+		let rect = eye.getBoundingClientRect();
+		// console.log('rectangle:',rect);
+
 		// Get center position of eyeball. 
-		let eyeX = rect.left + w/2;
-		let eyeY = rect.top + h/2;
-		// console.log('eyeX:',eyeX);
-		// console.log('eyeY:',eyeY);
+		let x = rect.left + w/2;
+		let y = rect.top + h/2;
+		// console.log('eye (x,y):', x,y);
 
-		// Get position of user's cursor (from top-left corner). 
-		let x = isTouchDevice() ? (event.touches[0].clientX) : (event.clientX);
-		let y = isTouchDevice() ? (event.touches[0].clientY) : (event.clientY);
-		// console.log('x:',x);
-		// console.log('y:',y);
+		// Return position. 
+		return {x:x,y:y};
+	}
 
-		// Get position differentials between pointer and eyeball. 
-		let dx = x - eyeX;
-		let dy = y - eyeY;
-		// console.log('dx:',dx);
-		// console.log('dy:',dy);
+	// Get position of pointer. 
+	function getPointerPosition() {
 
-		// Get angle of pointer position (in radians). 
+		// Check for touch screen device. 
+		let useTouch = isTouchDevice();
+
+		// Get position of user's pointer (from top-left corner). 
+		let x = useTouch ? (event.touches[0].clientX) : (event.clientX);
+		let y = useTouch ? (event.touches[0].clientY) : (event.clientY);
+		console.log('pointer (x,y):', x,y);
+
+		// Return position. 
+		return {x:x,y:y};
+
+		/***/
+
+		// Check for touch screen device. 
+		function isTouchDevice() {
+			try {
+				document.createEvent('TouchEvent');
+				return true;
+			}
+			catch(e) {
+				return false;
+			}
+		}
+	}
+
+	// Set rotation angle for eyeball. 
+	function setRotationAngle(eye, dx,dy) {
+
+		// Get angle of pointer position (from radians to degrees). 
 		let radAngle = Math.atan2(dx, dy);
-
-		// Get angle of pointer position (in degrees). 
 		let degAngle = radAngle * (180/Math.PI);
+		// console.log('Angle (pointer to eyeball):', radAngle,degAngle);
 
-		// Get rotation angle. 
+		// Get rotation angle for current eyeball. 
 		let degRotate = (-1) * degAngle + 180;
 
-		// Rotate iris of eye by rotation angle. 
-		eye.style.transform = "rotate("+ degRotate +"deg)";
-	}
-}
-
-// Check for touch screen. 
-function isTouchDevice() {
-	try {
-		document.createEvent('TouchEvent');
-		return true;
-	}
-	catch(e) {
-		return false;
+		// Rotate iris of current eyeball by given rotation angle. 
+		eye.style.transform = `rotate(${degRotate}deg)`;
 	}
 }
