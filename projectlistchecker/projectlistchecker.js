a3853229b60d82e0cff48ca0dfeadd8b5b207a4a
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-11-20 09:56:16 -0500

    Add files via upload

--- projectlistchecker/projectlistchecker.js
+++ projectlistchecker/projectlistchecker.js
@@ -1,30 +1,33 @@
 
 
 
-// Get result field for add list. 
-const addlistfield = document.querySelector('div#container main.main div.item div.listfield textarea#addlist');
-// console.log(addlistfield);
-
-// Get result field for remove list. 
-const removelistfield = document.querySelector('div#container main.main div.item div.listfield textarea#removelist');
-// console.log(removelistfield);
-
-// Get result field for original list. 
-const origlistfield = document.querySelector('div#container main.main div.item div.listfield textarea.listpaster');
-// console.log(origlistfield);
+// Get main container. 
+const container = document.querySelector('div#container');
+// console.log(container);
 
 
-// Get counter for add list. 
-const addcountfield = document.querySelector('div#container main.main div.item label.fieldlabel.add span.count');
-// console.log(addcountfield);
+// Get components of list field: id adds. 
+const addfield = {
+	list:document.querySelector('div#container main.main div.item div.listfield textarea#addlist'),
+	counter:document.querySelector('div#container main.main div.item label.fieldlabel.add span.count'),
+}
+// console.log(addfield);
 
-// Get counter for remove list. 
-const removecountfield = document.querySelector('div#container main.main div.item label.fieldlabel.remove span.count');
-// console.log(removecountfield);
 
-// Get counter for original list. 
-const origcountfield = document.querySelector('div#container main.main div.item label.fieldlabel.folder span.count');
-// console.log(origcountfield);
+// Get components of list field: id removals. 
+const removefield = {
+	list:document.querySelector('div#container main.main div.item div.listfield textarea#removelist'),
+	counter:document.querySelector('div#container main.main div.item label.fieldlabel.remove span.count'),
+}
+// console.log(removefield);
+
+
+// Get components of list field: original. 
+const origfield = {
+	list:document.querySelector('div#container main.main div.item div.listfield textarea.listpaster'),
+	counter:document.querySelector('div#container main.main div.item label.fieldlabel.folder span.count'),
+}
+// console.log(origfield);
 
 
 /*****/
@@ -32,42 +35,66 @@ const origcountfield = document.querySelector('div#container main.main div.item 
 
 // Paste full folder list. 
 function pasteFolderList() {
-	origlistfield.value = navigator.clipboard.readText();
+
+	// Paste full folder list. 
+	origfield['list'].value = navigator.clipboard.readText();
+
+	// Show indication of successful paste. 
 	this.classList.add('yes');
-	setTimeout( x=>{this.classList.remove('yes');} ,1500)
+
+	// Remove indication after a couple seconds. 
+	setTimeout( ()=>{this.classList.remove('yes');} , 1500);
 }
 
-// Find differences between lists. 
-function showDiffs() {
+// Check differences between project lists. 
+function checkProjects() {
 
 	// Get full folder list. 
-	let fullfolderlist = (origlistfield.value).split('\n');
-	origcountfield.textContent = fullfolderlist.length;
+	let fullfolderlist = (origfield['list'].value).split('\n');
+	origfield['counter'].textContent = fullfolderlist.length;
 	console.log('Full folder list:',fullfolderlist);
 
 	// Get list of project folders. 
-	let projectfolderlist = getDiffs(fullfolderlist,nullProjectIds);
+	let projectfolderlist = getDiffsList(fullfolderlist,nullProjectIds);
 
 	// Get list of ids to add (project folder w/o id listing). 
-	let idaddlist = getDiffs(projectfolderlist,allProjectIds);
-	let idaddhead = `${idaddlist.length} project id(s) missing. \nAdd project ids:`;
-	addcountfield.textContent = idaddlist.length;
-	console.log(idaddhead,idaddlist);
+	let idaddlist = getDiffsList(projectfolderlist,allProjectIds);
+	// Get count of add list. 
+	let addcount = idaddlist.length;
+	// Create label for id add list. 
+	let idaddlabel = `${addcount} project id(s) missing. \nAdd project ids:`;
+	addfield['counter'].textContent = addcount;
+	console.log(idaddlabel,idaddlist);
 
 	// Get list of ids to remove (id listing w/o project folder). 
-	let idremovelist = getDiffs(allProjectIds,fullfolderlist);
-	let idremovehead = `${idremovelist.length} extraneous project id(s). \nRemove project ids:`;
-	removecountfield.textContent = idremovelist.length;
-	console.log(idremovehead,idremovelist);
+	let idremovallist = getDiffsList(allProjectIds,fullfolderlist);
+	// Get count of removal list. 
+	let removalcount = idremovallist.length;
+	// Create label for id removal list. 
+	let idremovelabel = `${removalcount} extraneous project id(s). \nRemove project ids:`;
+	removefield['counter'].textContent = removalcount;
+	console.log(idremovelabel,idremovallist);
 	
 	// Display result lists. 
-	addlistfield.value = `${ idaddhead }\n${ idaddlist.join('\n') }`;
-	removelistfield.value = `${ idremovehead }\n${ idremovelist.join('\n') }`;
+	addfield['list'].value = `${ idaddlabel }\n${ idaddlist.join('\n') }`;
+	removefield['list'].value = `${ idremovelabel }\n${ idremovallist.join('\n') }`;
+
+	// Show appropriate signals. 
+	let good = addcount==0 && removalcount==0;
+	if(good) {
+		container.classList.remove('warn');
+		container.classList.add('good');
+	}
+	else {
+		container.classList.remove('good');
+		container.classList.add('warn');
+	}
+	container.classList.add('done');
 
 	/****/
 
-	// Get differences between lists. 
-	function getDiffs(includeList,excludeList) {
-		return includeList.filter( x=>!excludeList.includes(x) )
+	// Get differences between lists [ listA - listB ]. 
+	function getDiffsList(listA,listB) {
+		return listA.filter( item=>!listB.includes(item) );
 	}
 }
