3ceea66e5ee02fdee3481dd9fc6f83276c7b04d6
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-12-03 06:19:24 -0500

    Add files via upload

--- 3dshape/3dshape.js
+++ 3dshape/3dshape.js
@@ -11,6 +11,21 @@ const radiobtns = document.querySelectorAll('input[type=radio]');
 /*****/
 
 
+// Define face ids. 
+const cubefaceids = [
+	'spin',
+	'top',
+	'left',
+	'front',
+	'right',
+	'bottom',
+	'back',
+];
+
+
+/*****/
+
+
 // Enable shape movement. 
 enableMovement();
 
@@ -18,20 +33,38 @@ enableMovement();
 /*****/
 
 
-// Enable shape movement. 
+// Enable movement of cube. 
 function enableMovement() {
 
 	// Go thru each radio button. 
 	for(let btn of radiobtns) {
 
 		// Enable radio button clicks. 
-		btn.addEventListener('click',showFace);
-		
-		// Show face of selected cube side. 
-		function showFace(){
-			var cls = 'show-' + this.value;
-			cube.removeClass();
-			cube.addClass('shape cube '+cls)
+		btn.addEventListener('click',showCubeFace);
+	}
+
+	/****/
+
+	// Clear selection from cube. 
+	function clearCubeFaces() {
+
+		// Go thru each cube face id. 
+		for(let cfid of cubefaceids) {
+
+			// Remove face id from cube. 
+			cube.classList.remove(cfid);
 		}
+	}
+	
+	// Show face of selected cube side. 
+	function showCubeFace() {
+		console.log('this:',this);
+
+		// Get id of selected face. 
+		var selectedfaceid = this.value;
+	
+		// Clear selected cube side. 
+		clearCubeFaces();
+		cube.classList.add(selectedfaceid)
 	}
 }
