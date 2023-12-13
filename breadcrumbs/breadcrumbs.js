952a6bb92205061220eecb81ada4656abcc9f2b2
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2023-12-13 10:57:43 -0500

    Add files via upload

--- breadcrumbs/breadcrumbs.js
+++ breadcrumbs/breadcrumbs.js
@@ -1,70 +1,149 @@
 
 
 
-let navlinks = document.querySelectorAll('a.navlink');
-for(let link of navlinks) link.addEventListener('click', clickLink);
+// How can these procedures apply to a multi-page website ?
 
-// Handle link click. 
-function clickLink(event) {
 
-	// Get caption of clicked link. 
-	let clickedLinkCaption = event.currentTarget.innerText;
+// Get display for breadcrumbs. 
+const breadcrumbdisplay = document.querySelector('div#container main.main div#display');
 
-	// Get hierarchical order of pages. 
-	let result = getBreadcrumbs();
+// Get all navigation links. 
+const navlinks = document.querySelectorAll('a.navlink');
 
-	// Display hierarchical order of pages. 
-	let syllabus = document.getElementById('syllabus');
-	syllabus.innerHTML = result;
 
-	/*****/
+/*****/
 
-	// Get hierarchical order of pages. 
-	function getBreadcrumbs() {
 
-		// Create list of ancestor names. 
-		let ancestors = [];
-		let currentItem = event.currentTarget.parentElement;
-		while( isValidItem(currentItem) ) {
+// Load links. 
+loadLinks();
 
-			// Get name and url of current item. 
-			let navlink = currentItem.children[0];
-			let name = navlink.innerText;
-			let url = navlink.href;
+// Activate links. 
+activateLinks();
 
-			// Save name at beginning of list. 
-			ancestors.unshift({
-				name:name,
-				url:url,
-			});
 
-			// Move to next potential ancestor. 
-			currentItem = getAncestor(currentItem);
-		}
-		console.log('Breadcrumbs:', ancestors);
+/*****/
 
-		// Create string of names. 
-		let breadcrumbs = '<a class="navlink" href="javascript:void(0)">Home</a>';
-		for(let item of ancestors) {
-			// Add connecting arrow with item name. 
-			breadcrumbs += ' / '+`<a class="navlink" href="${item.url}">${item.name}</a>`;
-		}
-		// console.log('Breadcrumbs:', breadcrumbs);
 
-		return breadcrumbs;
+// TODO: Load links. 
+function loadLinks() {
 
-		/*****/
+	// 
+}
 
-		// Get parent item. 
-		function getAncestor(item) {
-			return item.parentElement.parentElement;
-		}
+// Activate links. 
+function activateLinks() {
 
-		// Check for valid item. 
-		function isValidItem(item) {
-			return item.classList.contains('navitem');
-		}
-
+	// Go thru all navigation links. 
+	for(let link of navlinks) {
+	
+		// Activate link click for navigation link. 
+		// link.addEventListener('mouseenter', showBreadcrumbs);
+		link.addEventListener('click', showBreadcrumbs);
 	}
 }
 
+// Display hierarchical order of pages for selected link. 
+function showBreadcrumbs(event) {
+
+	// Get selected link. 
+	let selectedlink = event.currentTarget;
+
+	// Show breadcrumb layout on display. 
+	breadcrumbdisplay.innerHTML = createBreadcrumbLayout();
+
+	/****/
+
+	// Get breadcrumb layout: hierarchical order of pages. 
+	function createBreadcrumbLayout() {
+		console.log('createBreadcrumbLayout');
+
+		// Get list of ancestor links. 
+		let ancestorlist = getAncestorLinks();
+		console.log('Breadcrumbs ancestor links:', ancestorlist);
+
+		// Initialize breadcrumb layout of ancestor name links. 
+		let breadcrumblayout = `
+		<!-- navlink -->
+		<a class="navlink" href="javascript:void(0)">Home</a>
+		<!-- /navlink -->`;
+
+		// Add icon to breadcrumb layout of ancestor name links. 
+		// breadcrumblayout = `
+		// <!-- navlink -->
+		// <a class="navlink" href="javascript:void(0)">${ getIconByTag('house') }</a>
+		// <!-- /navlink -->`;
+
+		// Go thru list of ancestor link items. 
+		for(let navitem of ancestorlist) {
+
+			// Add connecting arrow. 
+			breadcrumblayout += getIconByTag('rightcaret');
+
+			// Add link for current navigation item. 
+			breadcrumblayout += `
+			<!-- navlink -->
+			<a class="navlink" href="${navitem.url}">${navitem.name}</a>
+			<!-- /navlink -->`;
+		}
+		// console.log('Breadcrumbs:', breadcrumblayout);
+
+		// Return breadcrumb layout of ancestor name links. 
+		return breadcrumblayout;
+
+		/***/
+
+		// Get list of ancestor links. 
+		function getAncestorLinks() {
+
+			// Initialize list of ancestor links. 
+			let result = [];
+	
+			// Initialize current navigation item. 
+			let currentItem = selectedlink.parentElement;
+			console.log('Selected navigation item:',currentItem);
+
+			// Check if good to continue going. 
+			let continueGoing = isValidNavItem(currentItem);
+	
+			// Continue while item is valid navigation item. 
+			while( continueGoing ) {
+	
+				// Get associated navigation link. 
+				let navlink = currentItem.children[0];
+				// Get name and url of current item. 
+				let name = navlink.innerText;
+				let url = navlink.href;
+	
+				// Save link at top of ancestor list. 
+				result.unshift({
+					name:name,
+					url:url,
+				});
+	
+				// Move to next potential ancestor item. 
+				currentItem = getAncestor(currentItem);
+				console.log('Current item:',currentItem);
+			}
+
+			// Return list of ancestor links. 
+			return result;
+
+			/**/
+
+			// Get parent item of given navigation item. 
+			function getAncestor(navitem) {
+				return navitem.parentElement.parentElement;
+			}
+	
+			// Check for valid navigation item. 
+			function isValidNavItem(item) {
+				return item.classList.contains('navitem');
+			}
+		}
+
+		// Get icon by tag. 
+		function getIconByTag(tag) {
+			return `<svg class="icon ${tag}" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">${ iconData[tag] }</svg>`;
+		}
+	}
+}
---
+++ breadcrumbs/database/linkdatabase.js
@@ -0,0 +1,81 @@
+
+
+
+// Define hierarchy of links for site map. 
+const linkData = [
+	{
+		linkname:'Home',
+		linkurl:'javascript:void(0)',
+		sublinks:[
+			{
+				linkname:'Searching',
+				linkurl:'javascript:void(0)',
+				sublinks:[
+					{
+						linkname:'Linear Search',
+						linkurl:'javascript:void(0)',
+						sublinks:[],
+					},
+					{
+						linkname:'Binary Search',
+						linkurl:'javascript:void(0)',
+						sublinks:[],
+					},
+				],
+			},
+			{
+				linkname:'Sorting',
+				linkurl:'javascript:void(0)',
+				sublinks:[
+					{
+						linkname:'Bubble Sort',
+						linkurl:'javascript:void(0)',
+						sublinks:[],
+					},
+					{
+						linkname:'Merge Sort',
+						linkurl:'javascript:void(0)',
+						sublinks:[
+							{
+								linkname:'Recursive Merge Sort',
+								linkurl:'javascript:void(0)',
+								sublinks:[],
+							},
+							{
+								linkname:'Iterative Merge Sort',
+								linkurl:'javascript:void(0)',
+								sublinks:[],
+							},
+						],
+					},
+					{
+						linkname:'Selection Sort',
+						linkurl:'javascript:void(0)',
+						sublinks:[],
+					},
+					{
+						linkname:'Insertion Sort',
+						linkurl:'javascript:void(0)',
+						sublinks:[],
+					},
+				],
+			},
+			{
+				linkname:'Tree',
+				linkurl:'javascript:void(0)',
+				sublinks:[
+					{
+						linkname:'Binary Tree',
+						linkurl:'javascript:void(0)',
+						sublinks:[],
+					},
+					{
+						linkname:'Binary Search Tree',
+						linkurl:'javascript:void(0)',
+						sublinks:[],
+					},
+				],
+			},
+		],
+	},
+];
