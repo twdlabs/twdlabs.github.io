29417af36b7ac669dea8634c84d1016e7485c944
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2024-01-18 19:22:49 -0500

    Add files via upload

--- allProjectIds.js
+++ allProjectIds.js
@@ -1,52 +1,50 @@
 
 
-// List project names.
-const projectNames = [
-	'10000cards','100pure','360finance','3d','3dnav',
-	'accordion','accordion2','admin','admin2','affirm','agency','analogclock','angeldemon','animation','anime','apple','application','aspectratio','atg','atm','audio','autocomplete','autoslideshow',
-	'backToTop','band','banner','battlerap','bell','big3dcard','bizmath','blockmenu','blog','bloglist','blogsocial','blogtoggler','bodyshop','book','bookmark','books','bounce','box','boxshadow','breadcrumbs','bs5','bst','bubblesort','buttonpress','buy',
-	'calc','calculator','calendar','calendar2','cali','caliB','canvas','cardflipgallery3d','cardstacks','carnell','carnell2','carousel','carousel3d',
-	'chadpiff','charlimit','chart','chat','checkout','checkout2','chess','chooser','chooser-old','cis255','clearfix','clipboard','clock','clockspinner','clone','clubhouse','cluster','cluster1d','cms',
-	'code','cognitivebias','collapsible','colorpicker','colorpicker2','comingsoon','compound','contact','contactform','contextmenu', 'cookie','copyright','countdown','course','covid','crm','crud','crud2','cubecarousel','cutout','cv',
+
+// List ids for all projects.
+const allProjectIds = [
+	'10000cards','100pure','360finance',
+	'3dbox','3dcard','3dcardgallery','3dcarousel','3dnav','3dshape','3dsimulator','3dspincard',
+	'accordion','accordion2','adepa','admin','admin2','affiliate','affirm','agency','alphavantage','analogclock','angeldemon','animation','anime','apple','application','article','aspectratio','atg','atm','audio','autocomplete','autoslideshow',
+	'backToTop','band','banner','battlerap','bell','binarygame','bizmath','blockmenu','blog','bloglist','blogsocial','blogtoggler','bodyshop','book','bookmark','books','bounce','box','boxshadow','breadcrumbs','bs5','bst','bubblesort','buttonpress',
+	'calc','calculator','calendar','calendar2','cali','caliB','canvas','canvas2','cardstacks','carnell','carnell2','carousel',
+	'chadpiff','charlimit','chart','chat','chatpopup','checkout','checkout2','chess','chooser','chooser-old','cis255','clearfix','clipboard','clock','clockspinner','clone','clubhouse','cluster','cluster1d','cms',
+	'code','collapsible','colorpicker','colorpicker2','comingsoon','compound','contact','contactform','contextmenu', 'cookie','coordinates','copyright','countdown','covid','crm','cruchcalhoun','crud','crud2','cubecarousel','cutout',
 	'dashboard','db','decision','deck','delta','depth','desktop','device','diagram','dice','dollar','dotcollector','dotnav','dragndrop','dragndrop1','dragndroplist','dynamicnavbar',
-	'eazechain','editor','education','enter','essay','event','evolution','ezdev',
+	'eazechain','editor','education','enter','escapebrand','essay','event','evolution',
 	'fam','family','filter','finance','fitness','flashlight','flex','flexbreak','floatmenu','font','form','form3d','format','formvalidation','funnel',
-	'gallery','gallerywalk','gameboard','gametapes','gitcalendar','glow','goodz','gooey','gradient','graph','grid','grocery','grocery2','guess',
-	'hamburger','hero','hero2','hexagon','hovercard',
-	'icon','iglive','igprofile','igt','image','imagefilter','input','instagram','inventory',
+	'gallery','gallerywalk','gameboard','gametapes','gform','gitcalendar','glow','goodz','gooey','gradient','graph','grid','grocery','grocery2',
+	'hamburger','hero','hero2','herotrack','hexagon','hourglass','hovercard',
+	'iglive','igprofile','igt','image','input','instagram','inventory',
 	'jay','jenna','json',
-	'keyfinder','keyframes','keygenius','keypress','koan','kumba',
-	'lab','land','lander','laundry','layouts','leads','learner','levels','lightSwitch','linktree','livesearch','loader','loading','login','login2','logo',
-	'macbook','mandorespect','maskimage','masonry','math','matrix','medialibrary','megamenu','mementomori','mentalmodels','multimenu','merch','message','minion','multimenu','music','musicplayer','musicplaylist',
-	'navbar','navcircle','netflix','neuro67','news','notepad','notes','notesquares',
+	'keyfinder','keyframes','keypress','koan','kumba',
+	'lab','land','lander','laundry','layouts','leads','learner','levels','lightSwitch','linktree','livesearch','loader','loading','login','login1','login2','logo',
+	'macbook','manager','mandorespect','maskimage','masonry','math','matrix','medialibrary','megamenu','mementomori','mentalmodels','multimenu','merch','message','minion','mousetrail','multimenu','music','musicplayer','musicplayer2','musicplaylist',
+	'navbar','navcircle','navorbit','netflix','neuro67','news','notepad','notes','notesquares',
 	'online','orbit','overflow','overlay',
-	'pageNexter','pageslider','pagination','payment','periodictable','personality','perspective','pgi','phonecall','pixelart','pinyin','plax','plax2','podcast','pokeball','pong','popup','portfolio','portfolio1','postercircle','pricing','prime','probability','profilecard','progressbar','prototype',
-	'rating','rating2','reast','refresh','regexp','relativelink','resume','resume2','retool','robinhood','rolodex',
-	'safari','scale3d','scroller','scrollProgressBar','scrollsnap','searchbar','select','sermons','shape','share','sidenav','signin','skeleton','skills','sky','sliderboard','slideshow','slideshow2','smoothscroll','smosa',
-	'snapslider','socialbtns','socialmedia','solarsystem','sort','soundfx','spades','sphere','spinner','splash','squarespace','stepprogress','stepprogressbar','stocks','storage','storybrand','student','superforg','svsu','switcher','syntax',
-	'tabs','tabs3d','tagboard','tags','tagslider','tasks','taxes','team','template','testimonial','theme','theme1','theme2','thermometer','throttle','ticker','time','timeline','timeline2','timing','toast','toastprogress','todo','topnav','touch','traffic','travisscott','trifold3d','trig','tropicalghana','typer','typetest','typewriter',
-	'univ','unseenstrts','upload','upload1','urlparams','urlparams2','user','user2',
-	'valueslider','vanilla','video','video2','viewport','vidbg','vidbg2','voisey','vsl',
-	'wave','waves','wavyloader','wordcube','wordswitcher','wts','wts0','wts1','wts2',
+	'pagenexter','pageslider','paginator','payment','periodictable','personality','perspective','pgi','pixelart','pinyin','plax','plax1','plax2','plax3',
+	'podcast','pokeball','pong','popup','portfolio','portfolio0','postercircle','postercirclelibrary','pricing','prime','probability','profilecard','progressbar','projectlistchecker',
+	'quiz',
+	'rating','rating2','reast','reast2','refresh','regexp','resume','resume2','retool','robinhood','rolodex',
+	'safari','scale3d','scroller','scrollProgressBar','scrollsnap','searchbar','select','sermons','share','sidenav','skeleton','skills','sky','slideshow','slideshow2','smartphone','smoothscroll','smosa',
+	'snapslider','socialbtns','socialmedia','solarsystem','sort','soundfx','spades','sphere','spinner','splash','stepprogress','stepprogressbar','stocks','storage','storybrand','student','superforg','switch','switcher','syntax',
+	'tabs','tabs3d','tagboard','tagscroller','tagslider','tagtyper','tasks','template','template1','testimonial','theme','theme1','theme2','thermometer','throttle','ticker','time','timeline','timeline2','timing',
+	'toast','toastprogress','todo','topnav','touch','traffic','travel','3dtrifold','trig','tropicalghana','typecounter','typetest','typewriter',
+	'uitest','univ','unseenstrts','upload','upload1','urlparams','urlparams2','user','user2',
+	'valueslider','vanilla','video','video2','viewport','vidbg','vidbg2','voisey','vtabs',
+	'wave','waves','wavyloader','wordcube','wordswitcher','wts','wts1','wts2','wtslegacy','www',
 	'xhr',
 	'ytblp',
-	'zbar'
+	'zbar',
 ];
 
+// List ids for null projects (no index page).
+const nullProjectIds = [
+	'keygenius','liveserver','relativelink',
+	'svsu','travisscott','oscilloscope',
+	// 'resources','xyz','xyz',
+];
 
-
-// Create xyz. 
-function createXyz() {
-
-	// Initialize result. 
-	let result = '';
-
-	// Add to result. 
-	result += ``;
-
-	// Return result. 
-	return result;
-}
 
 
 
@@ -78,6 +76,6 @@ function createXyz() {
 // List public project names.
 // const projectPublicNames = [
 // 	'chooser','finance', 'math', 'music', 'probability', 'stocks',
-// 	'calc','cluster','tabs','db','diagram','flex','grid','igt','levels','matrix','mementomori','notes','phonecall','regexp','sidenav','tags','tasks','waves'
+// 	'calc','cluster','tabs','db','diagram','flex','grid','igt','levels','matrix','mementomori','notes','phonecall','regexp','sidenav','tagtyper','tasks','waves'
 // ];
 
