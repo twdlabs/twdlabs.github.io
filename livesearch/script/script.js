a8e3dab130c82f4d397da0012012155fb359111d
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2024-01-29 11:11:56 -0500

    Add files via upload

--- livesearch/script/script.js
+++ livesearch/script/script.js
@@ -2,16 +2,16 @@
 
 
 // Get open button. 
-const openbtn = document.getElementById('openbtn');
-// Get close button. 
-const closebtn = document.getElementById('closebtn');
+const openbtn = document.querySelector('div#container div.openbtn');
 
 // Get search overlay window. 
-const searchOverlay = document.getElementById('searchoverlay');
+const searchOverlay = document.querySelector('div#container div#searchoverlay');
+// Get close button. 
+const closebtn = document.querySelector('div#container div#searchoverlay section.head div.input div.closebtn');
 // Get search query field. 
-const searchField = document.getElementById('searchquery');
+const searchField = document.querySelector('div#container div#searchoverlay section.head div.input input#searchquery');
 // Get search results box. 
-const resultsBox = document.getElementById('resultsbox');
+const resultsBox = document.querySelector('div#container div#searchoverlay section.body div#resultsbox');
 
 
 /*****/
