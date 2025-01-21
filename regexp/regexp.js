6a7becbffd452d7e633f5c33ae1ec149b51a5a4d
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2025-01-21 18:13:45 -0500

    Add files via upload

--- regexp/regexp.js
+++ regexp/regexp.js
@@ -1,38 +1,54 @@
 
 
-$(document).ready(function() {
-	// Get height of text area. 
-	let h = $('textarea#testStrings').outerHeight();
-	// console.log('h',h);
 
-	// Assign same height to test strings backdrop. 
-	$('div#tsBackdrop').css('height',h);
-});
+// Get regular expression field. 
+const regexpfield = document.querySelector('div#container main.main section.regexp div.input input#regexp');
+
+// Get test string field. 
+const teststringfield = document.querySelector('div#container main.main section.teststring div.input textarea#teststring');
+
+// Get backdrop of test string field. 
+const teststringbackdrop = document.querySelector('div#container main.main section.teststring div.input div.backdrop');
+
+
+
+/*****/
+
+
+// Get height of text area. 
+let h = teststringfield.style.height;
+// console.log('h',h);
+
+// Assign same height to test strings backdrop. 
+teststringbackdrop.style.height = h;
+
+
+/*****/
 
 
 // Test regular expression. 
 function testRegex() {
+
 	// Get regular expression. 
-	let regexText = document.getElementById('regexp').value;
-	let regexp = new RegExp(regexText,'g');
+	let regexp = regexpfield.value;
+	let regexpobj = new RegExp(regexp,'g');
 
-	// Get test strings. 
-	let userText = document.getElementById('testStrings').value;
-	let testStrings = userText;//.split('\n');
-	console.log('\ntestStrings',testStrings);
+	// Get test string. 
+	let teststring = teststringfield.value;//.split('\n');
+	console.log('Test string:',teststring);
 
-	// Test the given strings for a match to the regexp. 
+	// Test the given strings for a match to the regular expression. 
 	let validity = [];
-	for(let i=0; i<testStrings.length; i++) {
-		let v = regexp.test();
-		console.log(v,testStrings[i]);
+	for(let i=0; i<teststring.length; i++) {
+		let v = regexpobj.test();
+		console.log(v,teststring[i]);
 		validity.push(v);
 	}
 
 	// Get text for test strings. 
-	let ts = $('#testStrings').val();
-	console.log('testStrings',ts);
+	let ts = teststringfield.value;
+	console.log('Test string:',ts);
 
 	// Copy test strings to test strings backdrop highlighter. 
-	$('div#tsBackdrop').html(ts);
+	teststringbackdrop.innerHTML = ts;
 }
