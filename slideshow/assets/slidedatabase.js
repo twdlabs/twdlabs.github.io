af08e53a8968e68b1c6040b3d48fc3d7dbee00e1
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2025-02-06 20:34:44 -0500

    Add files via upload

--- slideshow/assets/slidedatabase.js
+++ slideshow/assets/slidedatabase.js
@@ -4,20 +4,24 @@
 // Define slide show data. 
 // const slideshowdata = [
 // 	{
+// 		header:'Head A',
 // 		caption:'Slide A',
-// 		imageurl:'./../resources/images/scenery/img_5terre.jpg'
+// 		imageurl:'./../resources/images/sceneryw3/img_5terre.jpg'
 // 	},
 // 	{
+// 		header:'Head B',
 // 		caption:'Slide B',
-// 		imageurl:'./../resources/images/scenery/img_snow.jpg'
+// 		imageurl:'./../resources/images/sceneryw3/img_snow.jpg'
 // 	},
 // 	{
+// 		header:'Head C',
 // 		caption:'Slide C',
-// 		imageurl:'./../resources/images/scenery/img_mountains.jpg'
+// 		imageurl:'./../resources/images/sceneryw3/img_mountains.jpg'
 // 	},
 // 	{
+// 		header:'Head D',
 // 		caption:'Slide D',
-// 		imageurl:'./../resources/images/scenery/img_lights.jpg'
+// 		imageurl:'./../resources/images/sceneryw3/img_lights.jpg'
 // 	},
 // ];
 
@@ -25,35 +29,43 @@
 // Define slide show data. 
 const slideshowdata = [
 	{
+		header:'Head 1',
 		caption:'Slide 1',
-		imageurl:'./../resources/images/1.jpg'
+		imageurl:'./../resources/images/stock/1.jpg'
 	},
 	{
+		header:'Head 2',
 		caption:'Slide 2',
-		imageurl:'./../resources/images/2.jpg'
+		imageurl:'./../resources/images/stock/2.jpg'
 	},
 	{
+		header:'Head 3',
 		caption:'Slide 3',
-		imageurl:'./../resources/images/3.jpg'
+		imageurl:'./../resources/images/stock/3.jpg'
 	},
 	{
+		header:'Head 4',
 		caption:'Slide 4',
-		imageurl:'./../resources/images/4.jpg'
+		imageurl:'./../resources/images/stock/4.jpg'
 	},
 	{
+		header:'Head 5',
 		caption:'Slide 5',
-		imageurl:'./../resources/images/5.jpg'
+		imageurl:'./../resources/images/stock/5.jpg'
 	},
 	{
+		header:'Head 6',
 		caption:'Slide 6',
-		imageurl:'./../resources/images/6.jpg'
+		imageurl:'./../resources/images/stock/6.jpg'
 	},
 	{
+		header:'Head 7',
 		caption:'Slide 7',
-		imageurl:'./../resources/images/7.jpg'
+		imageurl:'./../resources/images/stock/7.jpg'
 	},
 	{
+		header:'Head 8',
 		caption:'Slide 8',
-		imageurl:'./../resources/images/8.jpg'
+		imageurl:'./../resources/images/stock/8.jpg'
 	},
 ];
