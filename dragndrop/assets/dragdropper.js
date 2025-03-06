6afea8d970b9aa9bd5bc97e0aecaaaf3135c4826
TWD Labs <25465781+twdlabs@users.noreply.github.com>
2025-03-06 13:45:46 -0500

    Add files via upload

--- dragndrop/assets/dragdropper.js
+++ dragndrop/assets/dragdropper.js
@@ -1,74 +1,144 @@
 
 
 
-// 
-class DragDropper {
+// Get all draggable items. 
+const alldragitems = document.querySelectorAll('div#container main.main div.box img.item');
 
-	// 
-	constructor(dragboxes,dragobj) {
+// Get all drop-boxes. 
+const alldropboxes = document.querySelectorAll('div#container main.main div.box');
 
-		// Save drag boxes. 
-		this.dragboxes = dragboxes;
 
-		// Save drag object. 
-		this.dragobj = dragobj;
+/*****/
 
-		// Activate drag boxes. 
-		this.activateBoxes();
+
+// Define link btwn single item and drop-boxes. 
+class DragDropMatrix {
+	
+	// Create new set of drag/drop relationships for given item. 
+	constructor(/* obj *//* ,boxes */) {
+
+		// Save draggable item. 
+		// this.dragitem = obj;
+		// obj.setAttribute('draggable','true');
+
+		// Save drop-boxes. 
+		// this.alldropboxes = boxes;
+
+		// Link draggable item to all drop-boxes. 
+		this.linkItemToBoxes();
 	}
 
-	// Activate drag destination boxes. 
-	activateBoxes() {
-	
-		// Go thru all boxes. 
-		for(let box of this.dragboxes) {
-			
-			// Handle drag passing. 
-			box.addEventListener('dragleave',passOverReceiving);
-		
-			// Handle prep for drag receipt. 
-			box.addEventListener('dragover',getReadyToReceive);
-			
-			// Handle drag receipt. 
-			box.addEventListener('drop',receiveIt.bind(this));
+	// Link draggable item to all drop-boxes. 
+	linkItemToBoxes() {
+		// console.log('Current item (this):',this);
+
+		// // Go thru each drag item. 
+		for(let dragitem of alldragitems) {
+
+			// Handle drag start. 
+			dragitem.addEventListener('dragstart',pickUpItem);
+			// Handle drag presence. 
+			// dragitem.addEventListener('drag',dragItem);
+			// Handle drag finish. 
+			dragitem.addEventListener('dragend',putDownItem);
 		}
-	
+
+		// Go thru each drop-box. 
+		// for(let box of this.alldropboxes) {
+		for(let dropbox of alldropboxes) {
+
+			// Handle drop receipt. 
+			dropbox.addEventListener('drop',receiveItem);
+
+			// Handle prep for drop receipt. 
+			dropbox.addEventListener('dragenter',prepDropbox);
+			dropbox.addEventListener('dragover',prepDropbox);
+			// Handle drop cancellation. 
+			dropbox.addEventListener('dragleave',cancelDropbox);
+		}
+
 		/****/
-	
-		// Pass over receiving element. 
-		function passOverReceiving(event) {
-		
-			// Get selected box. 
-			let selectedBox = event.currentTarget;
-	
-			// Update ready state. 
-			selectedBox.classList.remove('ready');
+
+		// Pick up draggable item. 
+		function pickUpItem(event) {
+			console.log('Picking up draggable item...',event.dataTransfer);
+
+			// Get selected drag-item. 
+			let selecteddragitem = event.currentTarget;
+			console.log('selecteddragitem:',selecteddragitem);
+
+			// Update drag state. 
+			selecteddragitem.classList.add('up');
+
+			// Save id of dragged item. 
+			let dragitemid = selecteddragitem.id;
+			console.log('dragitemid:',dragitemid);
+			event.dataTransfer.setData('dragitemid',dragitemid);
 		}
-	
-		// Get ready to receive element. 
-		function getReadyToReceive(event) {
-		
-			// Get selected box. 
-			let selectedBox = event.currentTarget;
-	
-			// Prevent default behavior to allow actual drop movement. 
+
+		// Drag draggable item. 
+		function dragItem(event) {
+
+			// Get selected drag-item. 
+			// let selecteddragitem = event.currentTarget;
+		}
+
+		// Put down draggable item. 
+		function putDownItem(event) {
+			console.log('Putting down draggable item...',event.dataTransfer);
+
+			// Get selected drag-item. 
+			let selecteddragitem = event.currentTarget;
+
+			// Update drag state. 
+			selecteddragitem.classList.remove('up');
+		}
+
+		// Receive item into drop-box. 
+		function receiveItem(event) {
+			console.log('Receiving item...',event);
+
+			// Get selected drop-box. 
+			let selectedbox = event.currentTarget;
+
+			// Get id of dragged item. 
+			let dragitemid = event.dataTransfer.getData('dragitemid');
+			console.log('dragitemid:',dragitemid);
+			// Get dragged item. 
+			let dragitem = document.getElementById(dragitemid);
+			// let dragitem = this.dragitem;
+			// let dragitem = alldragitems[2];
+
+			// Move draggable item into selected drop-box (if selected). 
+			selectedbox.appendChild(dragitem);
+
+			// Update ready state. 
+			selectedbox.classList.remove('ready');
+		}
+
+		// Prepare drop-box to receive. 
+		function prepDropbox(event) {
+			// console.log('Readying drop-box to receive...',event);
+
+			// Prevent default drag/drop behavior (to allow item movement). 
 			event.preventDefault();
-	
+
+			// Get selected drop-box. 
+			let selectedbox = event.currentTarget;
+
 			// Update ready state. 
-			selectedBox.classList.add('ready');
+			selectedbox.classList.add('ready');
 		}
-	
-		// Receive element. 
-		function receiveIt(event) {
-		
-			// Get selected box. 
-			let selectedBox = event.currentTarget;
-	
-			// Add drag object to selected box. 
-			selectedBox.appendChild(this.dragobj);
-	
+
+		// Cancel drop-box to receive. 
+		function cancelDropbox(event) {
+			// console.log('Un-readying drop-box...',event);
+
+			// Get selected drop-box. 
+			let selectedbox = event.currentTarget;
+
 			// Update ready state. 
-			selectedBox.classList.remove('ready');
+			selectedbox.classList.remove('ready');
 		}
 	}
 }
--- dragndrop/assets/script.js
+++ dragndrop/assets/script.js
@@ -1,15 +1,18 @@
 
 
 
-// Get all square boxes. 
-const squareboxes = document.querySelectorAll('div#container main.main div.box');
-
-// Get drag object. 
-const imageA = document.querySelector('div#container main.main div.box img.image');
-
+// 
 
 /*****/
 
 
-// Create new dragger dropper. 
-const d = new DragDropper(squareboxes, imageA);
+// Create new set of drag/drop relationships for item. 
+new DragDropMatrix(/* alldragitems[0] *//* ,alldropboxes */);
+
+
+// // Go thru each drag item. 
+// for(let item of alldragitems) {
+
+// 	// Create new set of drag/drop relationships for item. 
+// 	new DragDropMatrix(/* item *//* ,alldropboxes */);
+// }
