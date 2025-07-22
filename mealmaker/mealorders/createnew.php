5b8a30969f8167d88aa0fa0b8fd28428930376b4
Titus <25465781+twdlabs@users.noreply.github.com>
2025-07-21 20:15:55 -0400

    Add files via upload

--- mealmaker/mealorders/createnew.php
+++ mealmaker/mealorders/createnew.php
@@ -1,1 +1,142 @@
 
+<?php
+
+	// Start user session. 
+	session_start();
+
+	// Get functions to access server database. 
+	require_once('./../../sharedassets/script/config.php');
+	// Get functions to handle database queries. 
+	require_once('./../../sharedassets/script/query.php');
+	// // Get functions to access user input. 
+	require_once('./../../sharedassets/script/input.php');
+	// Get functions to display output. 
+	require_once('./../../sharedassets/script/output.php');
+	// Get functions to perform CRUD operations. 
+	require_once('./../../sharedassets/script/crudops.php');
+	// Get functions to perform user password operations. 
+	require_once('./../../sharedassets/script/userpasswdops.php');
+
+	// Get metadata for database tables. 
+	require_once('./../assets/database/database.php');
+	require_once('./../assets/database/databasequery.php');
+	// Get metadata for database table icons. 
+	require_once('./../assets/database/tableicons.php');
+	// Get functions to perform CRUD operations. 
+	require_once('./../assets/script/crudops.php');
+	// Get functions to perform user operations. 
+	require_once('./../assets/script/userops.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="mobile-web-app-capable" content="yes"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="apple-mobile-web-app-capable" content="yes"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<title>Baba's Bagel</title>
+
+		<!-- Main Stylesheet (shared) -->
+		<link href="./../../sharedassets/style/style.css" rel="stylesheet" type="text/css"/>
+		<!-- Navbar Stylesheet (shared) -->
+		<link href="./../../sharedassets/style/navbar.css" rel="stylesheet" type="text/css"/>
+		<!-- Query Stylesheet (shared) -->
+		<link href="./../../sharedassets/style/query.css" rel="stylesheet" type="text/css"/>
+		<!-- Data Table Stylesheet (shared) -->
+		<link href="./../../sharedassets/style/datatable.css" rel="stylesheet" type="text/css"/>
+		<!-- CRUD Form Stylesheet (shared) -->
+		<link href="./../../sharedassets/style/crudform.css" rel="stylesheet" type="text/css"/>
+
+		<!-- Main Stylesheet -->
+		<link href="./../assets/style/style.css?v=20250712" rel="stylesheet" type="text/css"/>
+		<link href="./../assets/style/navbar.css?v=20250717" rel="stylesheet" type="text/css"/>
+		<!-- <style></style> -->
+
+		<script type="text/javascript">
+			// console.log('Server data:', <?php print isset($_SERVER) ? json_encode($_SERVER) : null; ?>);
+			console.log('Session data:', <?php print isset($_SESSION) ? json_encode($_SESSION) : null; ?>);
+			console.log('Response data (GET):', <?php print isset($_GET) ? json_encode($_GET) : null; ?>);
+			console.log('Response data (POST):', <?php print isset($_POST) ? json_encode($_POST) : null; ?>);
+		</script>
+	</head>
+
+	<body>
+
+		<!-- #container -->
+		<div id="container">
+
+			<?php
+
+				// Display opening of query arena. 
+				include('./../assets/module/queryarena/open.php');
+
+				// Connect to server database. 
+				$db = openDb('mealmaker');
+
+				// Set C.R.U.D. operation parameters. 
+				$crudopid = 'create';
+				$crudtableid = 'mealorders';
+
+				// Handle new C.R.U.D. operation. 
+				$queryresult = handleCrudOps($crudopid,$crudtableid);
+				$querysuccess = $queryresult['summary']['roweffect'];
+				$querysummary = json_encode( $queryresult['summary'] );
+				printToPage("querysummary: $querysummary<br>querysuccess: $querysuccess");
+				// print "querysummary: $querysummary<br>";
+				// print "querysuccess: $querysuccess<br>";
+
+				// Display closing of query arena. 
+				include('./../assets/module/queryarena/close.php');
+
+				// Display navbar. 
+				include('./../assets/module/navbar.php');
+			?>
+
+			<!-- item -->
+			<section class="item">
+
+				<?php if( $querysuccess ): ?>
+
+					<!-- head -->
+					<h2 class="head">Query successful!</h2>
+					<!-- /head -->
+
+				<?php else: ?>
+
+					<!-- head -->
+					<h2 class="head">Query unsuccessful</h2>
+					<!-- /head -->
+
+				<?php endif; ?>
+
+				<!-- returnlink -->
+				<a class="returnlink" href="./../?view=<?=$crudtableid?>">Proceed</a>
+				<!-- /returnlink -->
+
+			</section>
+			<!-- /item -->
+
+		</div>
+		<!-- /#container -->
+
+		<!-- Toggler Script -->
+		<script src="./../../sharedassets/script/toggler.js" type="text/javascript"></script>
+
+		<script type="text/javascript">
+
+			// Close query arena (when page fully loaded). 
+			closeQueryArena();
+		</script>
+
+	</body>
+
+</html>
+
+<?php
+
+	// Disconnect server database. 
+	closeDb($db);
+?>
