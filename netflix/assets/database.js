3f0b54ea811e75c6075880037315335998ae9be9
Titus <25465781+twdlabs@users.noreply.github.com>
2026-06-09 21:58:23 -0400

    Add files via upload

--- netflix/assets/database.js
+++ netflix/assets/database.js
@@ -3,47 +3,158 @@
 
 // Get media source. 
 // const mediasourcelist = galleryMediaData;
-const mediasourcelist = blogDataList;
+let mediasourcelist = blogDataList;
 
-// Define contents of slide rows. 
-const sliderowcontents = [
+// Define data for each slide row. 
+const mediadatamatrix = [
+];
+// Define data for each slide row. 
+const samplemediadatamatrix = [
 	{
 		rowtitle:'Genre Category A',
-		mediaindexes:[],
+		rowtitle:'Category A',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category B',
-		mediaindexes:[],
+		rowtitle:'Category B',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category C',
-		mediaindexes:[],
+		rowtitle:'Category C',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category D',
-		mediaindexes:[],
+		rowtitle:'Category D',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category E',
-		mediaindexes:[],
+		rowtitle:'Category E',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category F',
-		mediaindexes:[],
+		rowtitle:'Category F',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category G',
-		mediaindexes:[],
+		rowtitle:'Category G',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category H',
-		mediaindexes:[],
+		rowtitle:'Category H',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 	{
 		rowtitle:'Genre Category I',
-		mediaindexes:[],
+		rowtitle:'Category I',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
+	},
+	{
+		rowtitle:'Genre Category J',
+		rowtitle:'Category J',
+		mediaindexlist:[],
+		mediasourcelist:/* tossItIn */(mediasourcelist),
 	},
 ];
+
+
+// Select project library as data source. 
+// selectProjectLibrary();
+
+
+// Select project library as data source (by category). 
+function selectProjectLibrary() {
+
+	// Go thru each category in library. 
+	for( let category of projectCategoryData ) {
+
+		// Initialize list of projects for given category. 
+		let projectlist = [];
+
+		// Go thru each project in category. 
+		for( let projectid of category['groupitemsidlist'] ) {
+
+			// Get data for current project. 
+			let projectdata = getProjectById(projectid);
+
+			// Get image from gallery. 
+			// let randomimage = getRandomImage();
+			// console.log('randomimage:',randomimage);
+
+			// Add data of current project to list. 
+			projectlist.push( {
+				// pid:projectid,
+				title: projectdata ? projectdata['projectname'] : '[Undefined Project]',
+				// thumbnailurl: randomimage['thumbnailurl'],
+				thumbnailurl: getRandomImage()['thumbnailurl'],
+			} );
+		}
+
+		// 
+		mediadatamatrix.push( {
+			rowicon:category['groupicontag'],
+			rowtitle:category['groupname'],
+			mediasourcelist:projectlist,
+		} );
+	}
+
+	// Choose random image from gallery. 
+	function getRandomImage() {
+
+		// Get number of images in gallery. 
+		let imagecount = galleryMediaData.length;
+		// console.log('imagecount:',imagecount);
+
+		// Get random number. 
+		let r = Math.random();
+		// console.log('r:',r);
+
+		// Get random index from gallery image list. 
+		let randomindex = Math.floor( r * imagecount );
+		// console.log('randomindex:',randomindex);
+
+		// Get random image from gallery. 
+		return galleryMediaData[randomindex];
+	}
+
+	// // Go thru each collection. 
+	// for( let collection of projectCollectionData ) {
+
+	// 	// 
+	// 	mediadatamatrix.push( {
+	// 		rowicon:collection['groupicontag'],
+	// 		rowtitle:collection['groupname'],
+	// 		mediasourcelist:[],
+	// 	} );
+
+	// 	// Go thru each category. 
+	// 	for( let category of xyz ) {
+
+	// 		// 
+
+	// 		// Go thru each projects. 
+	// 		for( let project of xyz ) {
+
+	// 			// 
+	// 		}
+	// 	}
+	// }
+}
 
 
 // Define index of selected show episode. 
@@ -92,3 +203,14 @@ const episodeData = [
 		duration:30
 	},
 ];
+
+
+// TODO: Toss it in from given list of media. 
+function tossItIn(srcmedialist) {
+
+	// 
+	let newlength = getRandomIndex(srcmedialist.length);
+
+	// 
+	let newmedialist = [];
+}
